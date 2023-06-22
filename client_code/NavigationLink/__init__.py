aebccc333dc375204ada254dfe965db18c8d63bc
brooke@anvil.works <brooke@anvil.works>
2023-06-22 13:11:55 +0000

    Edited NavigationLink; Deleted Form1

--- client_code/NavigationLink/__init__.py
+++ client_code/NavigationLink/__init__.py
@@ -5,11 +5,3 @@ class NavigationLink(NavigationLinkTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
-  @property
-  def icon(self):
-      return self._icon
-
-  @my_prop.setter
-  def icon(self, value):
-      self._icon = value
