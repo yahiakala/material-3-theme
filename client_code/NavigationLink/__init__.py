06016a7c655b44ba89a56cfc9f98fbaf563f6b2b
brooke@anvil.works <brooke@anvil.works>
2023-06-22 14:36:54 +0000

    Edited settings, NavigationLink and StandardPageLayout

--- client_code/NavigationLink/__init__.py
+++ client_code/NavigationLink/__init__.py
@@ -5,3 +5,23 @@ class NavigationLink(NavigationLinkTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    print(self.url)
+
+      
+  @property
+  def url(self):
+    return self._url
+
+  @url.setter
+  def url(self, value):
+    print(value)
+    self._url = value
+
+  def form_show(self, **event_args):
+    """This method is called when the HTML panel is shown on the screen"""
+
+    self.call_js('setHref', self.url)
+
+
+    
+    
