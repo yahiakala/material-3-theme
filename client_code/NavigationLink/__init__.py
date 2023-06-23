081d6432a31930b77025ef84cd5bc8d48d952418
brooke@anvil.works <brooke@anvil.works>
2023-06-23 12:18:28 +0000

    Edited NavigationLink

--- client_code/NavigationLink/__init__.py
+++ client_code/NavigationLink/__init__.py
@@ -20,11 +20,18 @@ class NavigationLink(NavigationLinkTemplate):
 
   @property
   def icon(self):
+    print(self.dom_nodes['link-icon'].classList[1])
     return self.dom_nodes['link-icon'].classList[1]
 
   @icon.setter
   def icon(self, value):
+    print(value)
+    value = value.replace(":", "-")
+    print(value)
     if value:
-      self.dom_nodes['navigation-link'].href = value
+      self.dom_nodes['link-icon'].className = ""
+      self.dom_nodes['link-icon'].classList.add("fa")
+      self.dom_nodes['link-icon'].classList.add(value)
+      
 
 
