cfe2bb38dbf5480a9f338d6995d30c840017d89d
brooke@anvil.works <brooke@anvil.works>
2023-06-23 11:52:28 +0000

    Edited NavigationLink and StandardPageLayout

--- client_code/NavigationLink/__init__.py
+++ client_code/NavigationLink/__init__.py
@@ -6,9 +6,20 @@ class NavigationLink(NavigationLinkTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    print(self.dom_nodes['navigation-link'].classList[0])
+    print(self.icon)
 
   @property
   def url(self):
+    return self.dom_nodes['navigation-link'].href
+
+  @url.setter
+  def url(self, value):
+    if value:
+      self.dom_nodes['navigation-link'].href = value
+
+  @property
+  def icon(self):
     return self.dom_nodes['navigation-link'].href
 
   @url.setter
