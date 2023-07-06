30753e7b46cbd24e57377aac6e300faa2781914b
patricia@anvil.works <patricia@anvil.works>
2023-07-06 10:19:36 +0000

    Deleted Button

--- client_code/Button/__init__.py
+++
@@ -1,9 +0,0 @@
-from ._anvil_designer import ButtonTemplate
-from anvil import *
-
-class Button(ButtonTemplate):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
-
-    # Any code you write here will run before the form opens.
