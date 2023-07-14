e9baee2a6df9d93411ab4c0b9e9f13cd4a747d73
brooke@anvil.works <brooke@anvil.works>
2023-07-14 11:48:20 +0000

    Fix theme_color_to_css function

--- client_code/Functions.py
+++ client_code/Functions.py
@@ -1,8 +1,12 @@
 from anvil import *
+
 def m3_alert(title=None, content=None, buttons="OK", icon=None, dividers=False, centered=False):
   pass
 
 def theme_color_to_css(color:str):
-  color = color.strip('theme:')
-  return app.theme_colors[color]
+  if color.startswith('theme:'):
+    color = color.strip('theme:')
+    return app.theme_colors[color]
+  else:
+    return color
   
