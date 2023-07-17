401d3dbb0e9560a1004eab323f2054d0d278fcb8
patricia@anvil.works <patricia@anvil.works>
2023-07-17 15:07:00 +0000

    Edited theme.css and TextField

--- client_code/TextField/__init__.py
+++ client_code/TextField/__init__.py
@@ -8,6 +8,19 @@ class TextField(TextFieldTemplate):
 
     # Any code you write here will run before the form opens.
 
+  @property
+  def max_characters(self):
+    return self._max_characters
+
+  @max_characters.setter
+  def max_characters(self, value):
+    character_count = self.dom_nodes['text-field-character-count']
+    if value:
+      character_count.style.display = "block"
+      character_count.innerHTML = value
+
+
+
   
 
   
