667d5e0d6caaf0ce59fdc1df0016d9f01c7f07f3
brooke@anvil.works <brooke@anvil.works>
2023-07-21 17:01:39 +0000

    Edited Components.Checkbox, theme.css, anvil-m3/checkbox.css and TestPage

--- client_code/Components/Checkbox/__init__.py
+++ client_code/Components/Checkbox/__init__.py
@@ -6,4 +6,38 @@ class Checkbox(CheckboxTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
+  @property
+  def disabled(self):
+    return self.dom_nodes['anvil-m3-checkbox'].disabled
+
+  @disabled.setter
+  def disabled(self, value):
+    self.dom_nodes['anvil-m3-checkbox'].disabled = value
+
+  @property
+  def selected(self):
+    return self.dom_nodes['anvil-m3-checkbox'].checked
+
+  @selected.setter
+  def selected(self, value):
+    self.dom_nodes['anvil-m3-checkbox'].checked = value
+
+  @property
+  def label(self):
+    return self.dom_nodes['anvil-m3-checkbox-label'].innerText
+
+  @label.setter
+  def label(self, value):
+    self.dom_nodes['anvil-m3-checkbox-label'].innerText = value
+
+  @property
+  def error(self):
+    return self._error
+
+  @error.setter
+  def error(self, value):
+    self.dom_nodes['anvil-m3-checkbox-container'].classList.remove('anvil-m3-checkbox-error')
+    self._error = value
+    if value:
+      self.dom_nodes['anvil-m3-checkbox-container'].classList.add('anvil-m3-checkbox-error')
+      self._error = value
