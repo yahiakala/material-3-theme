e52f5d8a8b66a4210d9c086ddce06ac3509053e7
brooke@anvil.works <brooke@anvil.works>
2023-07-24 13:16:31 +0000

    add anvil-m3 prefixes and update color vars for switch css

--- client_code/Components/Switch/__init__.py
+++ client_code/Components/Switch/__init__.py
@@ -16,14 +16,14 @@ class Switch(SwitchTemplate):
 
   @enabled_icon.setter
   def enabled_icon(self, value):
-    link_icon = self.dom_nodes['enabled-switch-icon']
-    switch_slider = self.dom_nodes['switch-slider']
+    link_icon = self.dom_nodes['anvil-m3-enabled-switch-icon']
+    switch_slider = self.dom_nodes['anvil-m3-switch-slider']
     self._enabled_icon = value
     link_icon.classList.remove("material-symbols-outlined")
     if value:
       link_icon.classList.add("material-symbols-outlined")
       link_icon.innerText = value
-      switch_slider.classList.add('has-enabled-icon')
+      switch_slider.classList.add('anvil-m3-has-enabled-icon')
   
   @property
   def disabled_icon(self):
@@ -31,33 +31,20 @@ class Switch(SwitchTemplate):
 
   @disabled_icon.setter
   def disabled_icon(self, value):
-    link_icon = self.dom_nodes['disabled-switch-icon']
-    switch_slider = self.dom_nodes['switch-slider']
+    link_icon = self.dom_nodes['anvil-m3-disabled-switch-icon']
+    switch_slider = self.dom_nodes['anvil-m3-switch-slider']
     self._disabled_icon = value
     
     link_icon.classList.remove("material-symbols-outlined")
     if value:
       link_icon.classList.add("material-symbols-outlined")
       link_icon.innerText = value
-      switch_slider.classList.add('has-disabled-icon')
+      switch_slider.classList.add('anvil-m3-has-disabled-icon')
 
   @property
   def enabled(self):
-    return self.dom_nodes['switch-input'].checked
+    return self.dom_nodes['anvil-m3-switch-input'].checked
 
   @enabled.setter
   def enabled(self, value):
-    self.dom_nodes['switch-input'].checked = value
-
-  #query selectors not acting as expected. Will return to this. component is finished, otherwise
-  @property
-  def enabled_background_color(self):
-    return self._enabled_background_color
-
-  @enabled_background_color.setter
-  def enabled_background_color(self, value):
-    if value:
-      self._enabled_background_color = value
-      color = theme_color_to_css(value)
-      # jQuery('.switch-input:checked + .switch-slider').css('background-color', color)
-      print(self.dom_nodes['switch'].querySelector('.switch-input:checked + .switch-slider'))
+    self.dom_nodes['anvil-m3-switch-input'].checked = value
