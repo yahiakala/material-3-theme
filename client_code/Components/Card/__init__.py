0738edb6743b1a4c0fe3941b85b2ef402b2acd7b
char@anvil.works <char@anvil.works>
2023-07-25 08:37:28 +0000

    Edited Components.Card

--- client_code/Components/Card/__init__.py
+++ client_code/Components/Card/__init__.py
@@ -3,44 +3,33 @@ from anvil import *
 
 class Card(CardTemplate):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.printList()
-    print()
-    self.hasClass("anvil-m3-card")
-
-  def printList(self):
-    classes = self.dom_nodes['anvil-m3-card'].classList
-    for c in classes:
-      print(c)
 
   @property
   def appearance(self):
     return self._appearance
-
+    
   @appearance.setter
   def appearance(self, value):
     card = self.dom_nodes['anvil-m3-card']
-    if value:
-      card.classList.add(value)
-    # needs to replace instead of adding.
+    classes = 
 
   @property
   def interactive(self):
     return self._interactive
-
   @interactive.setter
   def interactive(self, value):
     card = self.dom_nodes['anvil-m3-card']
-    # if value is false
-    #   check if card includes interactive. remove if it does.
-    # if value is true
-    #   check if card includes interactive. add it if it doesn't.
-
-
+    classes = card.classList
+    print(classes)
+    
+    alreadyInteractive = self.hasClass('interactive')
+    if (alreadyInteractive and not value):
+      print("remove value")
+    elif (not alreadyInteractive and value):
+      card.classList.add(value)  
   def hasClass(self, className):
     classes = self.dom_nodes['anvil-m3-card'].classList
-    if className in classes:
-      print("YESSS")
+    return classes.contains(className)
       
   
