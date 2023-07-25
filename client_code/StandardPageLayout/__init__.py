9bcaa2bd9c7a498d6955235ee0f5d775fafac58e
brooke@anvil.works <brooke@anvil.works>
2023-07-25 15:16:18 +0000

    Edited StandardPageLayout

--- client_code/StandardPageLayout/__init__.py
+++ client_code/StandardPageLayout/__init__.py
@@ -2,10 +2,13 @@ from ._anvil_designer import StandardPageLayoutTemplate
 from anvil import *
 from ..Functions import innerText_property
 
+
 class StandardPageLayout(StandardPageLayoutTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+
+
 
     # Any code you write here will run before the form opens.
   
