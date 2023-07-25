fbf3304add132dac0cb3dde274a5e0c9c1738712
brooke@anvil.works <brooke@anvil.works>
2023-07-25 16:31:03 +0000

    Edited StandardPageLayout

--- client_code/StandardPageLayout/__init__.py
+++ client_code/StandardPageLayout/__init__.py
@@ -1,21 +1,28 @@
 from ._anvil_designer import StandardPageLayoutTemplate
 from anvil import *
 from ..Functions import innerText_property
+from anvil.js import window
 
 
 class StandardPageLayout(StandardPageLayoutTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    var appBar = $('.anvil-m3-top-app-bar')
-    var navDrawerOpenBtn = $('.anvil-m3-drawer-open-btn')
-    var navDrawerCloseBtn = $('.anvil-m3-drawer-close-btn')
-    var navRail = $('.anvil-m3-navigation-rail')
-    var navDrawerScrim = $('.anvil-m3-navigation-drawer-scrim')
+    
+    self.app_bar = self.dom_nodes['anvil-m3-top-app-bar']
+    self.nav_drawer_open_btn = self.dom_nodes['anvil-m3-drawer-open-btn']
+    self.nav_drawer_close_btn = self.dom_nodes['anvil-m3-drawer-close-btn']
+    self.nav_rail = self.dom_nodes['anvil-m3-navigation-rail']
+    self.nav_drawer_scrim = self.dom_nodes['anvil-m3-navigation-drawer-scrim']
 
-
-
-    # Any code you write here will run before the form opens.
+    window.document.addEventListener('scroll', self.add_scroll_class)
+                                  
+  def add_scroll_class(self):
+    if self.app_bar.hasClass('anvil-m3-scrolled'):
+      if window.scrollY == 0:
+        self.app_bar.removeClass('anvil-m3-scrolled')
+    else:
+      self.app_bar.addClass('anvil-m3-scrolled')    
   
   @property
   def navigation_rail_collapse_to(self):
