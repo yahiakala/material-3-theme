c9584d95555fd62046ee3c46f104bb31336b8a35
brooke@anvil.works <brooke@anvil.works>
2023-07-27 10:47:04 +0000

    Edited theme.css and StandardPageLayout

--- client_code/StandardPageLayout/__init__.py
+++ client_code/StandardPageLayout/__init__.py
@@ -31,7 +31,7 @@ class StandardPageLayout(StandardPageLayoutTemplate):
 
   def hide_nav_drawer(self, e):
     self.nav_rail.classList.remove('anvil-m3-shown')
-    window.setTimeout(lambda: self.nav_rail.style.width = '0px', 250)
+    window.setTimeout(lambda: self.nav_rail.style.setProperty('width', '0px'), 250)
     # self.nav_rail.style.width = '0px'
     # self.nav_drawer_close_btn.display = 'none'
 
