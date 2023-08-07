e7318f71605a285011fa940b7f297c4956c8ced1
brooke@anvil.works <brooke@anvil.works>
2023-08-07 14:28:52 +0000

    Edited TestPage

--- client_code/TestPage/__init__.py
+++ client_code/TestPage/__init__.py
@@ -6,7 +6,7 @@ class TestPage(TestPageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.indeter
+    self.indeterminate_checkbox.checked = None
 
 
     # Any code you write here will run before the form opens.
