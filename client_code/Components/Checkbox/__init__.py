0157981b7e8f34cfc676af99be0e25c873ad4756
brooke@anvil.works <brooke@anvil.works>
2023-08-07 17:48:21 +0000

    Edited Components.Checkbox

--- client_code/Components/Checkbox/__init__.py
+++ client_code/Components/Checkbox/__init__.py
@@ -29,7 +29,7 @@ class Checkbox(CheckboxTemplate):
         "execute": lambda: anvil.designer.start_inline_editing(self, "text", self.dom_nodes['anvil-m3-checkbox-label'])
       }
     },
-                         
+                       
                          ]
     return di
   
