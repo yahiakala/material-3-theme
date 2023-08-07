fb8480619daf538a49f315324a3614f21641927d
brooke@anvil.works <brooke@anvil.works>
2023-08-07 14:29:46 +0000

    Edited TestPage

--- client_code/TestPage/__init__.py
+++ client_code/TestPage/__init__.py
@@ -9,20 +9,6 @@ class TestPage(TestPageTemplate):
     self.indeterminate_checkbox.checked = None
 
     # Any code you write here will run before the form opens.
-
-  def button_3_click(self, **event_args):
-    """This method is called when the component is clicked"""
-    alert("clicked button in elevated card")
-
-  def group_a1_click(self, **event_args):
-    self.label_5.text = self.radio_button_1.get_group_value()
-
-  def group_t2_click(self, **event_args):
-    self.label_6.text = self.radio_button_5.get_group_value()
-
-  def group_qwer_click(self, **event_args):
-    self.label_7.text = self.radio_button_8.get_group_value()
-
   
 
 
