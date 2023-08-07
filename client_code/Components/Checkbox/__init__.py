ed267e27e5148918ae1b83da3776c59eeab9513f
brooke@anvil.works <brooke@anvil.works>
2023-08-07 14:18:46 +0000

    Edited Components.Checkbox

--- client_code/Components/Checkbox/__init__.py
+++ client_code/Components/Checkbox/__init__.py
@@ -57,7 +57,7 @@ class Checkbox(CheckboxTemplate):
   @allow_indeterminate.setter
   def allow_indeterminate(self, value):
     self._allow_indeterminate = value
-    
+    if 
 
   @property
   def error(self):
