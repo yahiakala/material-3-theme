a0883d6125996ca2a4afab4787e028f199155e8e
brooke@anvil.works <brooke@anvil.works>
2023-08-07 18:08:15 +0000

    Edited Components.Checkbox

--- client_code/Components/Checkbox/__init__.py
+++ client_code/Components/Checkbox/__init__.py
@@ -32,7 +32,7 @@ class Checkbox(CheckboxTemplate):
     {
       "type": "whole_component",
       "title": "Checked",
-      "icon": "check",
+      "icon": "eye",
       "default": True,
       "callbacks": {
         "execute": lambda: handle_click(self)
