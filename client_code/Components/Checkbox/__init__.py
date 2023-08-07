ad8ab6c8002a5e36266477920504f4907a2dccd3
brooke@anvil.works <brooke@anvil.works>
2023-08-07 09:21:54 +0000

    Edited Components.Checkbox

--- client_code/Components/Checkbox/__init__.py
+++ client_code/Components/Checkbox/__init__.py
@@ -22,6 +22,9 @@ class Checkbox(CheckboxTemplate):
     if anvil.designer.in_designer and not self.text:
       print('yes')
       self.text = anvil.designer.get_design_name(self)
+      self.dom_nodes['anvil-m3-checkbox-label'].style.fontStyle = "italic"
+      self.dom_nodes['anvil-m3-checkbox-label'].style.color = "#bbb"
+      
 
   enabled = enabled_property('anvil-m3-checkbox')
   selected = checked_property('anvil-m3-checkbox')
