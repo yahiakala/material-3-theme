5ddda417aa9fc00b47387c613884d8b42fd8fa3b
brooke@anvil.works <brooke@anvil.works>
2023-08-08 10:31:27 +0000

    Edited Components.Checkbox

--- client_code/Components/Checkbox/__init__.py
+++ client_code/Components/Checkbox/__init__.py
@@ -46,7 +46,6 @@ class Checkbox(CheckboxTemplate):
   visible = visible_property('anvil-m3-checkbox-container', 'inline-flex')
   underline = underline_property('anvil-m3-checkbox-label')
   italic = italic_property('anvil-m3-checkbox-label')
-  font_size = style_property('anvil-m3-checkbox-label', 'fontSize')
   
   
   @property
