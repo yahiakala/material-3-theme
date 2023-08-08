1d2d5684ac6a9c8877adb34615db5e2916ee1d7f
brooke@anvil.works <brooke@anvil.works>
2023-08-08 13:34:03 +0000

    Edited Components.Checkbox

--- client_code/Components/Checkbox/__init__.py
+++ client_code/Components/Checkbox/__init__.py
@@ -16,7 +16,8 @@ class Checkbox(CheckboxTemplate):
     """This method is called when the HTML panel is shown on the screen"""
     if anvil.designer.in_designer and not self.text:
       self.text = anvil.designer.get_design_name(self)
-      self.italic
+      self.italic = True
+      self.text_color = ""
 
   def _anvil_get_design_info_(self, as_layout=False):
     di = super()._anvil_get_design_info_(as_layout)
