0d9d6f193e5e4c7b6aa4905fc1fed5966d3b11ea
brooke@anvil.works <brooke@anvil.works>
2023-08-08 10:16:54 +0000

    Edited Components.Checkbox

--- client_code/Components/Checkbox/__init__.py
+++ client_code/Components/Checkbox/__init__.py
@@ -44,7 +44,7 @@ class Checkbox(CheckboxTemplate):
 
   enabled = enabled_property('anvil-m3-checkbox')
   visible = visible_property('anvil-m3-checkbox-container', 'inline-flex')
-  
+  underline = sty
   
   @property
   def checked(self):
