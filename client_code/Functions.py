b3dfebed5b65f5a6f611d34781cc826e88a9acac
brooke@anvil.works <brooke@anvil.works>
2023-08-08 14:04:12 +0000

    Edited Functions

--- client_code/Functions.py
+++ client_code/Functions.py
@@ -131,7 +131,7 @@ def italic_property(dom_node_name):
     self._italic = value
     if value:
       self.dom_nodes[dom_node_name].style.fontStyle = 'italic'
-    else:
+    elif value == False:
       self.dom_nodes[dom_node_name].style.fontStyle = 'normal'
 
   return property(getter, setter)
