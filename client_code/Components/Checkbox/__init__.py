b19296cbe6fdfee1a92ff589398e9db143e00685
brooke@anvil.works <brooke@anvil.works>
2023-08-08 14:33:43 +0000

    Edited Components.Checkbox

--- client_code/Components/Checkbox/__init__.py
+++ client_code/Components/Checkbox/__init__.py
@@ -48,7 +48,7 @@ class Checkbox(CheckboxTemplate):
   font = style_property('anvil-m3-checkbox-label', 'fontFamily')
   text_color = color_property('anvil-m3-checkbox-label', 'color')
   background = color_property('anvil-m3-checkbox-container', 'backgroundColor')
-  text = 
+  text = innerText
 
   @property
   def checkbox_color(self):
