54c80cb75e3b30024de5fb8750ca1e7caf6aabe9
brooke@anvil.works <brooke@anvil.works>
2023-08-08 14:33:27 +0000

    Edited Components.Checkbox

--- client_code/Components/Checkbox/__init__.py
+++ client_code/Components/Checkbox/__init__.py
@@ -81,15 +81,6 @@ class Checkbox(CheckboxTemplate):
     else:
       self.dom_nodes['anvil-m3-checkbox'].checked = value
       
-  @property
-  def text(self):
-    return self._text
-
-  @text.setter
-  def text(self, value):
-    self._text = value
-    if value:
-      self.updateText(value, False)
 
   @property
   def allow_indeterminate(self):
