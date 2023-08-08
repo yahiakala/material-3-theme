c2b543fe474386a177efaa51312d0f40c4ad84c2
brooke@anvil.works <brooke@anvil.works>
2023-08-08 12:51:43 +0000

    Edited TestPage

--- client_code/TestPage/__init__.py
+++ client_code/TestPage/__init__.py
@@ -11,6 +11,8 @@ class TestPage(TestPageTemplate):
     self.indeterminate_checkbox_3.checked = None
     self.indeterminate_checkbox_4.checked = None
 
+    
+
     # Any code you write here will run before the form opens.
 
   def radio_button_2_click(self, **event_args):
