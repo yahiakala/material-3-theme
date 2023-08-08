f4d27e35a4a4acfe7961ebc07cbe3032be2d6ef2
brooke@anvil.works <brooke@anvil.works>
2023-08-08 14:48:56 +0000

    Edited TestPage

--- client_code/TestPage/__init__.py
+++ client_code/TestPage/__init__.py
@@ -6,20 +6,6 @@ class TestPage(TestPageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.indeterminate_checkbox_1.checked = None
-    self.indeterminate_checkbox_2.checked = None
-    self.indeterminate_checkbox_3.checked = None
-    self.indeterminate_checkbox_4.checked = None
-
-    # Any code you write here will run before the form opens.
-
-  def radio_button_2_click(self, **event_args):
-    pass
-
-  def checkbox_2_click(self, **event_args):
-    """This method is called when the component is checked or unchecked"""
-    pass
-
 
   
 
