a1baab413b3e2a4265c44178d83f1052aac7492d
brooke@anvil.works <brooke@anvil.works>
2023-08-08 10:23:58 +0000

    Edited Components.Checkbox

--- client_code/Components/Checkbox/__init__.py
+++ client_code/Components/Checkbox/__init__.py
@@ -1,6 +1,6 @@
 from ._anvil_designer import CheckboxTemplate
 from anvil import *
-from ...Functions import enabled_property, style_property, visible_property, underline_property
+from ...Functions import enabled_property, style_property, visible_property, underline_property, italic_property
 import anvil.designer
 
 #TODO: 
