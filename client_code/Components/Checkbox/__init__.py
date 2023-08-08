b2589a1c86d429296d7954e3bdcb025251aaff4f
brooke@anvil.works <brooke@anvil.works>
2023-08-08 15:03:27 +0000

    Edited Components.Checkbox

--- client_code/Components/Checkbox/__init__.py
+++ client_code/Components/Checkbox/__init__.py
@@ -55,7 +55,7 @@ class Checkbox(CheckboxTemplate):
 
   @align.property
   def align(self, value):
-    self.dom_nodes['anvil-m3-checkbox-component']
+    self.dom_nodes['anvil-m3-checkbox-component'].style.ju
 
   @property
   def checkbox_color(self):
