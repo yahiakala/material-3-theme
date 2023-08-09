d9348b77028191851a294942ad4a18dbb6072dce
patricia@anvil.works <patricia@anvil.works>
2023-08-09 11:00:33 +0000

    Edited Functions

--- client_code/Functions.py
+++ client_code/Functions.py
@@ -79,15 +79,6 @@ def enabled_property(dom_node_name):
 
   return property(getter, setter)
 
-def disabled_property(dom_node_name):
-  def getter(self):
-    return self.dom_nodes[dom_node_name].disabled
-
-  def setter(self, value):
-    self.dom_nodes[dom_node_name].disabled = value
-
-  return property(getter, setter)
-
 def name_property(dom_node_name):
   def getter(self):
     return self.dom_nodes[dom_node_name].name
