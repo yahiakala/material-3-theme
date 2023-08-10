44d5dda770f1f0bc762f54eba96a11ec4cadb209
brooke@anvil.works <brooke@anvil.works>
2023-08-10 14:29:10 +0000

    Edited Components.Heading

--- client_code/Components/Heading/__init__.py
+++ client_code/Components/Heading/__init__.py
@@ -85,7 +85,7 @@ class Heading(HeadingTemplate):
     display = self.dom_nodes['anvil-m3-heading-display']
     headline = self.dom_nodes['anvil-m3-heading-headline']
     title = self.dom_nodes['anvil-m3-heading-title']
-    self.dom_nodes['anvil-m3-heading']
+    self.dom_nodes['anvil-m3-heading-container'].
     if value == 'display':
       display.style.display = 'block'
       headline.style.display = 'none'
