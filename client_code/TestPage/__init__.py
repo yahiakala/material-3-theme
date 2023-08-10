1497beaeded826eeaf183bcd902efa4d2b2ad8fb
brooke@anvil.works <brooke@anvil.works>
2023-08-10 16:48:53 +0000

    Edited TestPage

--- client_code/TestPage/__init__.py
+++ client_code/TestPage/__init__.py
@@ -7,6 +7,11 @@ class TestPage(TestPageTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
+  def button_1_click(self, **event_args):
+    """This method is called when the component is clicked"""
+    pass
+
+
 
   
 
