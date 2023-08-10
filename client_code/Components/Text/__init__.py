02dcf8a01b472da802296e267e52b8cef439fb69
brooke@anvil.works <brooke@anvil.works>
2023-08-10 17:04:29 +0000

    Edited Components.Text

--- client_code/Components/Text/__init__.py
+++ client_code/Components/Text/__init__.py
@@ -3,7 +3,7 @@ from anvil import *
 import anvil.designer
 from ...Functions import visible_property, underline_property, italic_property, style_property, color_property, innerText_property, bold_property, font_size_property
 
-
+#TODO: figure out what to do with line height
 
 class Text(TextTemplate):
   def __init__(self, **properties):
