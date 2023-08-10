29f9fda62bbe018835387632eaa1624d643dbef9
brooke@anvil.works <brooke@anvil.works>
2023-08-10 16:49:42 +0000

    Edited Components.Text

--- client_code/Components/Text/__init__.py
+++ client_code/Components/Text/__init__.py
@@ -46,6 +46,7 @@ class Text(TextTemplate):
   @font_size.setter
   def font_size(self, value):
     self._font_size = value
+    print(value)
     if value:
       self.dom_nodes['anvil-m3-text'].style.fontSize = f'{value}px'
       self.dom_nodes['anvil-m3-text-container'].style.fontSize = f'{value}px'
