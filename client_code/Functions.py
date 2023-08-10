60bb2a63dbe954faac5338eea26901f2d0808217
brooke@anvil.works <brooke@anvil.works>
2023-08-10 17:10:47 +0000

    Edited Functions

--- client_code/Functions.py
+++ client_code/Functions.py
@@ -18,6 +18,7 @@ def color_property(dom_node_name, style_prop):
   def setter(self, value):
     if value:
       self.dom_nodes[dom_node_name].style[style_prop] = theme_color_to_css(value)
+    else:
 
   return property(getter, setter)
 
