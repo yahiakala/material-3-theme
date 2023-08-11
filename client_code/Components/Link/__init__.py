d513dfa7b13ad6be6ef6b55dd18a15d7b465e03c
brooke@anvil.works <brooke@anvil.works>
2023-08-11 09:04:00 +0000

    Created Components.Link; Deleted Components.Form1

--- client_code/Components/Link/__init__.py
+++ client_code/Components/Link/__init__.py
@@ -1,7 +1,7 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import LinkTemplate
 from anvil import *
 
-class Form1(Form1Template):
+class Link(LinkTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
