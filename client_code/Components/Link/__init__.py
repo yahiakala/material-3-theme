a73ca4142bbd14232e8868ecd2c2ecc7f8d7d9cd
brooke@anvil.works <brooke@anvil.works>
2023-08-11 09:27:40 +0000

    Edited Components.Link

--- client_code/Components/Link/__init__.py
+++ client_code/Components/Link/__init__.py
@@ -11,3 +11,16 @@ class Link(LinkTemplate):
     """This method is called when the HTML panel is shown on the screen"""
     if anvil.designer.in_designer and not self.text:
       self.text = anvil.designer.get_design_name(self)
+
+  def _anvil_get_design_info_(self, as_layout=False):
+    di = super()._anvil_get_design_info_(as_layout)
+    di['interactions'] = [{
+      "type": "whole_component",
+      "title": "Edit text",
+      "icon": "edit",
+      "default": True,
+      "callbacks": {
+        "execute": lambda: anvil.designer.start_inline_editing(self, "text", self.dom_nodes['anvil-m3-text'])
+      }
+    }]
+    return di
