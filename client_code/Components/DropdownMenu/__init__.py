c1d4984f17f2c15fda545599368840e12d7971e4
char@anvil.works <char@anvil.works>
2023-10-09 13:32:54 +0000

    Edited TestPage and Components.DropdownMenu

--- client_code/Components/DropdownMenu/__init__.py
+++ client_code/Components/DropdownMenu/__init__.py
@@ -1,7 +1,7 @@
 from ._anvil_designer import DropdownMenuTemplate
 from anvil import HtmlTemplate
 from anvil import *
-from ...Functions import underline_property, italic_property, style_property, color_property, innerText_property, bold_property, font_size_property
+# from ...Functions import underline_property, italic_property, style_property, color_property, innerText_property, bold_property, font_size_property
 
 class DropdownMenu(DropdownMenuTemplate):
   def __init__(self, **properties):
@@ -11,4 +11,20 @@ class DropdownMenu(DropdownMenuTemplate):
     # Any code you write here will run before the form opens.
   
   visible = HtmlTemplate.visible
-    
+  
+  @property
+  def enabled(self):
+    return self._enabled
+  @enabled.setter
+  def enabled(self, value):
+    self._enabled = value
+    self.text_field.enabled = value
+  
+  @property
+  def font(self):
+    return self._font
+  @font.setter
+  def font(self, value):
+    self._font = value
+    self.text_field.font = value
+    # this should maybe change the font of the menuItems too
