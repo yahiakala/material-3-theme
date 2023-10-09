1d704bdb2d0067e4000df667a05804a5d9b90ce3
char@anvil.works <char@anvil.works>
2023-10-09 15:01:57 +0000

    Edited Components.DropdownMenu

--- client_code/Components/DropdownMenu/__init__.py
+++ client_code/Components/DropdownMenu/__init__.py
@@ -1,6 +1,8 @@
 from ._anvil_designer import DropdownMenuTemplate
 from anvil import HtmlTemplate
 from anvil import *
+from anvil.js.window import document
+import anvil.designer
 # from ...Functions import underline_property, italic_property, style_property, color_property, innerText_property, bold_property, font_size_property
 
 class DropdownMenu(DropdownMenuTemplate):
@@ -15,6 +17,11 @@ class DropdownMenu(DropdownMenuTemplate):
     # textFieldNode = self.text_field.dom_nodes['text-field'];
     
     self.handle_box_click = self.handle_box_click
+    self.remove_shield_handler = self.remove_shield_handler
+    
+    self.shield = document.createElement("div")
+    self.shield.classList.toggle("anvil-m3-menu-clickShield", True)
+    
     self.add_event_handler("x-anvil-page-added", self.on_mount)
     self.add_event_handler("x-anvil-page-removed", self.on_cleanup)
 
@@ -23,12 +30,12 @@ class DropdownMenu(DropdownMenuTemplate):
   # click event listener on box to toggle open or closed menu
   
     # document.addEventListener('keydown', self.handle_keyboard_events)
-    # self.shield.addEventListener('click', self.remove_shield_handler)
+    self.shield.addEventListener('click', self.remove_shield_handler)
     # self.menuNode.addEventListener('click', self.child_clicked)
   def on_cleanup(self, **event_args):
-    textFieldNode.removeEventListener('click', self.handle_box_click)
+    self.text_field.dom_nodes['text-field'].removeEventListener('click', self.handle_box_click)
     # document.removeEventListener('keydown', self.handle_keyboard_events)
-    # self.shield.removeEventListener('click', self.remove_shield_handler)
+    self.shield.removeEventListener('click', self.remove_shield_handler)
     # self.menuNode.removeEventListener('click', self.child_clicked)
   
   
@@ -52,9 +59,30 @@ class DropdownMenu(DropdownMenuTemplate):
     # this should maybe change the font of the menuItems too
 
   def handle_box_click(self, event):
+    # todo: figure out how to get it to now do the textfield interactions
+    event.preventDefault()
+    event.stopPropagation()
+    
     self.set_menu_visibility()
 
   def set_menu_visibility(self, value = None):
     if (value is None):
       value = not self.menu.visible
     self.menu.visible = value
+    if value:
+      if not anvil.designer.in_designer:
+          self.place_shield()
+
+  def place_shield(self):
+    if not document.contains(self.shield):
+      document.body.appendChild(self.shield)
+      document.body.style.overflow = "hidden"
+
+  def remove_shield_handler(self, event):
+    self.remove_shield()
+    self.set_menu_visibility(False)
+    
+  def remove_shield(self):
+    if document.contains(self.shield):
+      document.body.removeChild(self.shield)
+      document.body.style.removeProperty("overflow")
