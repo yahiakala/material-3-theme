38e8590a9670a8df10349b2a9c2361fdc3f5e2b7
char@anvil.works <char@anvil.works>
2023-10-09 13:21:22 +0000

    Edited anvil-m3/menu.css and Components.DropdownMenu

--- client_code/Components/DropdownMenu/__init__.py
+++ client_code/Components/DropdownMenu/__init__.py
@@ -1,5 +1,7 @@
 from ._anvil_designer import DropdownMenuTemplate
+from anvil import HtmlTemplate
 from anvil import *
+from ...Functions import underline_property, italic_property, style_property, color_property, innerText_property, bold_property, font_size_property
 
 class DropdownMenu(DropdownMenuTemplate):
   def __init__(self, **properties):
@@ -7,3 +9,6 @@ class DropdownMenu(DropdownMenuTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+  
+  visible = HtmlTemplate.visible
+    
