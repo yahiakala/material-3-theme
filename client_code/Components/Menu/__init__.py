10ca97b50b90317d6b6ab1ad65e8627e8e61e535
char@anvil.works <char@anvil.works>
2023-10-10 10:51:40 +0000

    Created TestModule; Edited AnotherPage, Components, Components.Button, Components.ButtonMenu and 19 more

--- client_code/Components/Menu/__init__.py
+++ client_code/Components/Menu/__init__.py
@@ -4,6 +4,7 @@
 
 from ._anvil_designer import MenuTemplate
 from anvil import *
+import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
--- client_code/TestPage/__init__.py
+++ client_code/TestPage/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import TestPageTemplate
 from anvil import *
+import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
@@ -10,10 +11,11 @@ from anvil.tables import app_tables
 class TestPage(TestPageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
+    self.drop_down_1.item = anvil.server.call('get_jobs')
+    
     self.init_components(**properties)
-    self.selecty_items = [(cat['Items'], cat) for cat in app_tables.test.search()]
-    self.drop_down_1.items = self.selecty_items
-    self.drop_down_2 = self.selecty_items
+    # self.selecty_items = [(cat['Items'], cat) for cat in app_tables.test.search()]
+    # self.drop_down_1.items = self.selecty_items
 
   def select_font(self, value):
     print(f"font face {value} selected")
