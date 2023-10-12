ea24c346f356432ecabcb88e0d5c89e9fc6f4b48
brooke@anvil.works <brooke@anvil.works>
2023-10-12 14:33:42 +0000

    Edited Functions and TestPage

--- client_code/Functions.py
+++ client_code/Functions.py
@@ -44,6 +44,7 @@ def innerText_property(dom_node_name):
     return self.dom_nodes[dom_node_name].innerText
 
   def setter(self, value):
+    print('setting innerText: ', value)
     self.dom_nodes[dom_node_name].innerText = value
 
   return property(getter, setter)
