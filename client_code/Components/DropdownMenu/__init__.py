0c149717debff6acd75e9bbce3e1b0d1f24252b7
char@anvil.works <char@anvil.works>
2023-10-16 09:15:21 +0000

    Edited Components.DropdownMenu

--- client_code/Components/DropdownMenu/__init__.py
+++ client_code/Components/DropdownMenu/__init__.py
@@ -316,19 +316,10 @@ class DropdownMenu(DropdownMenuTemplate):
       selection.add_event_handler('click', handle_selection_click)
       self.menu.add_component(selection, slot="anvil-m3-menu-slot")
 
- 
 # DESIGNER INTERACTIONS
   def _anvil_get_design_info_(self, as_layout=False):
     design_info = super()._anvil_get_design_info_(as_layout)
-    design_info["interactions"] = [
-      # {
-      #   "type": "designer_events",
-      #   "callbacks": {
-      #     "onSelectDescendent": self._on_select_descendent,
-      #     "onSelectOther": self._on_select_other
-      #   }
-      # },
-      {
+    design_info["interactions"] = [{
       "type": "whole_component",
       "title": "Visible",
       "icon": "add", #TODO: eye icon
@@ -369,14 +360,8 @@ class DropdownMenu(DropdownMenuTemplate):
       "callbacks": {
         "execute": self.set_alignment
       }
-    },]
+    }]
     return design_info
-
-  # def _on_select_descendent(self):
-  #   self.set_visibility(True)
-
-  # def _on_select_other(self):
-  #   self.set_visibility(False)
 
   def toggle_visible(self):
     self.visible = not self.visible
