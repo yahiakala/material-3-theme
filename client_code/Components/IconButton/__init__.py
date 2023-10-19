9908e8909d6735e758a86a7fe2cc1da53cac477f
char@anvil.works <char@anvil.works>
2023-10-19 09:30:11 +0000

    Edited anvil-m3/iconbutton.css, TestPage and Components.IconButton

--- client_code/Components/IconButton/__init__.py
+++ client_code/Components/IconButton/__init__.py
@@ -4,10 +4,28 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from ...Functions import innerText_property
 
 class IconButton(IconButtonTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
+  @property
+  def selected(self):
+    return self._selected
+  @selected.setter
+  def selected(self, value):
+    self._selected = value
+    # todo update the css of the dom
+
+  @property
+  def appearance(self):
+    return self._appearance
+  @appearance.setter
+  def appearance(self, value):
+    self._appearance = value
+    # todo update the css of the dom
+
+  icon = innerText_property('anvil-m3-iconbutton-icon')
+    
