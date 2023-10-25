56a61686536581e04d1538926c2459cfab58f86b
char@anvil.works <char@anvil.works>
2023-10-25 10:47:51 +0000

    Edited Components.ToggleIconButton and TestPage

--- client_code/Components/ToggleIconButton.py
+++ client_code/Components/ToggleIconButton.py
@@ -4,9 +4,9 @@ from ..Functions import property_with_callback
 icon_button_properties = getattr(IconButton, "_anvil_properties_", [])
 
 selected_property = {"name": "selected", "type": "boolean", "group": "Attributes", "important": True, "default_value": False}
-selected_border_property = {"name": "selected_border", "type": "string", "group": "Selected Icon Style" } #, "description": "Border style when component is in the selected state. Accepts any valid CSS border value."}
-selected_background_property = {"name": "selected_background", "type": "color", "group": "Selected Style" } #, "description": "Background color when component is in the selected state."}
-selected_icon_color_property = {"name": "selected_icon_color", "type": "color", "group": "Selected Style" } #, "description": "Icon color when component is in the selected state."}
+selected_border_property = {"name": "selected_border", "type": "string", "group": "Style" } #, "description": "Border style when component is in the selected state. Accepts any valid CSS border value."}
+selected_background_property = {"name": "selected_background", "type": "color", "group": "Style" } #, "description": "Background color when component is in the selected state."}
+selected_icon_color_property = {"name": "selected_icon_color", "type": "color", "group": "Style" } #, "description": "Icon color when component is in the selected state."}
 
 class ToggleIconButton(IconButton):
   _anvil_properties_ = [selected_property, selected_border_property, selected_background_property, selected_icon_color_property, *icon_button_properties]
@@ -15,15 +15,16 @@ class ToggleIconButton(IconButton):
     
     self.dom_nodes['anvil-m3-iconbutton-container'].classList.toggle("toggle", True)
     self.dom_nodes['anvil-m3-iconbutton-icon'].classList.toggle("toggle", True)
+    self.apply_styles(self.selected)
     
   # Properties 
   def selected_setter(self, value):
     self.dom_nodes['anvil-m3-iconbutton-container'].classList.toggle("selected", value)
     self.dom_nodes['anvil-m3-iconbutton-icon'].classList.toggle("selected", value)
-    self.apply_styles()
+    self.apply_styles(value)
     
-  def apply_styles(self):
-    if self.selected:
+  def apply_styles(self, value):
+    if value:
       self.dom_nodes['anvil-m3-iconbutton-container'].style.backgroundColor = self.selected_background
       self.dom_nodes['anvil-m3-iconbutton-container'].style.border = self.selected_border
       self.dom_nodes['anvil-m3-iconbutton-icon'].style.color = self.selected_icon_color
@@ -32,7 +33,16 @@ class ToggleIconButton(IconButton):
       self.dom_nodes['anvil-m3-iconbutton-container'].style.border = self.border
       self.dom_nodes['anvil-m3-iconbutton-icon'].style.color = self.icon_color
 
+
+  # 
+  # border = style_property('anvil-m3-iconbutton-container', 'border')
+  # icon_color = color_property('anvil-m3-iconbutton-icon', 'color')
+  # background = color_property('anvil-m3-iconbutton-container', 'backgroundColor')
+  
   selected = property_with_callback("selected", selected_setter)
+  border = property_with_callback("border", apply_styles)
+  icon_color = property_with_callback("icon_color", apply_styles)
+  background = property_with_callback("background", apply_styles)
   selected_border = property_with_callback("selected_border", apply_styles)
   selected_background = property_with_callback("selected_background", apply_styles)
   selected_icon_color = property_with_callback("selected_icon_color", apply_styles)
@@ -45,5 +55,5 @@ class ToggleIconButton(IconButton):
   # This will be deprecated v soon.
   def _anvil_get_design_info_(self, as_layout=False): 
     design_info = super()._anvil_get_design_info_(as_layout)
-    design_info["propertyDescriptions"] = [selected_property] + design_info["propertyDescriptions"]
+    design_info["propertyDescriptions"] = [selected_property, selected_border_property, selected_background_property, selected_icon_color_property] + design_info["propertyDescriptions"]
     return design_info
