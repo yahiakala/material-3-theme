e0c3527d118accdbf7d7814e4d930f1354e875e7
char@anvil.works <char@anvil.works>
2023-10-25 14:06:05 +0000

    Edited Components.ToggleIconButton and TestPage

--- client_code/Components/ToggleIconButton.py
+++ client_code/Components/ToggleIconButton.py
@@ -4,9 +4,9 @@ from ..Functions import property_with_callback
 icon_button_properties = getattr(IconButton, "_anvil_properties_", [])
 
 selected_property = {"name": "selected", "type": "boolean", "group": "Attributes", "important": True, "default_value": False}
-selected_border_property = {"name": "selected_border", "type": "string", "group": "Style" } #, "description": "Border style when component is in the selected state. Accepts any valid CSS border value."}
-selected_background_property = {"name": "selected_background", "type": "color", "group": "Style" } #, "description": "Background color when component is in the selected state."}
-selected_icon_color_property = {"name": "selected_icon_color", "type": "color", "group": "Style" } #, "description": "Icon color when component is in the selected state."}
+selected_border_property = {"name": "selected_border", "type": "string", "group": "Toggled Style" , "description": "Border style when component is in the selected state. Accepts any valid CSS border value."}
+selected_background_property = {"name": "selected_background", "type": "color", "group": "Toggled Style" , "description": "Background color when component is in the selected state."}
+selected_icon_color_property = {"name": "selected_icon_color", "type": "color", "group": "Toggled Style" , "description": "Icon color when component is in the selected state."}
 
 class ToggleIconButton(IconButton):
   _anvil_properties_ = [selected_property, selected_border_property, selected_background_property, selected_icon_color_property, *icon_button_properties]
@@ -54,4 +54,79 @@ class ToggleIconButton(IconButton):
   def _anvil_get_design_info_(self, as_layout=False): 
     design_info = super()._anvil_get_design_info_(as_layout)
     design_info["propertyDescriptions"] = [selected_property, selected_border_property, selected_background_property, selected_icon_color_property] + design_info["propertyDescriptions"]
+    design_info["interactions"] = [
+      {
+      "type": "whole_component_multi",
+      "title": "Align",
+      "options": [{
+        "name": "Left Align",
+        "id": "left",
+        "icon" : "align-left",
+      },{
+        "name": "Left Center",
+        "id": "center",
+        "icon" : "align-center",
+      },{
+        "name": "Left Right",
+        "id": "right",
+        "icon" : "align-right",
+      }],
+      "callbacks": {
+        "execute": self.setAlignment
+      }
+    },
+      {
+      "type": "whole_component",
+      "title": "Visible",
+      "icon": "add", #TODO: eye icon
+      "callbacks": {
+        "execute": self.toggle_visible
+      }
+    }, {
+      "type": "whole_component",
+      "title": "Enable",
+      "icon": "add", #TODO: power icon
+      "callbacks": {
+        "execute": self.toggle_enabled
+      } 
+    },{
+      "type": "region",
+      "bounds": self.dom_nodes['anvil-m3-iconbutton-component'],
+      "callbacks": {
+        "doubleClick": self.toggle_selected
+      }
+    } ]
     return design_info
+
+  def setAlignment(self, value):
+    self.align = value
+    anvil.designer.update_component_properties(self, {'align': self.align})
+  def toggle_visible(self):
+    self.visible = not self.visible
+    anvil.designer.update_component_properties(self, {'visible': self.visible})
+  def toggle_enabled(self):
+    self.enabled = not self.enabled
+    anvil.designer.update_component_properties(self, {'enabled': self.enabled})
+  def toggle_selected(self):
+    self.selected = not self.selected
+    anvil.designer.update_component_properties(self, {'selected': self.selected})
+
+
+"""
+ def toggle_selected(self):
+    self.selected = not self.selected
+    anvil.designer.update_component_properties(self, {'selected': self.selected})
+
+  def toggle_enabled(self):
+    self.enabled = not self.enabled
+    anvil.designer.update_component_properties(self, {'enabled': self.enabled})
+
+  def toggle_visible(self):
+    self.visible = not self.visible
+    anvil.designer.update_component_properties(self, {'visible': self.visible})
+
+  def setAlignment(self, value):
+    self.align = value
+    anvil.designer.update_component_properties(self, {'align': self.align})
+
+"""
