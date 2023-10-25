baf14a224246a583e295d7c7fe9c370b1c9d9a36
char@anvil.works <char@anvil.works>
2023-10-25 10:33:49 +0000

    Edited Components.ToggleIconButton

--- client_code/Components/ToggleIconButton.py
+++ client_code/Components/ToggleIconButton.py
@@ -4,9 +4,9 @@ from ..Functions import property_with_callback
 icon_button_properties = getattr(IconButton, "_anvil_properties_", [])
 
 selected_property = {"name": "selected", "type": "boolean", "group": "Attributes", "important": True, "default_value": False}
-selected_border_property = {"name": "selected_border", "type": "string", "group": "Selected Style", "description": "Border style when component is in the selected state. Accepts any valid CSS border value."}
-selected_background_property = {"name": "selected_background", "type": "color", "group": "Selected Style", "description": "Background color when component is in the selected state."}
-selected_icon_color_property = {"name": "selected_icon_color", "type": "color", "group": "Selected Style", "description": "Icon color when component is in the selected state."}
+selected_border_property = {"name": "selected_border", "type": "string", "group": "Selected Icon Style" } #, "description": "Border style when component is in the selected state. Accepts any valid CSS border value."}
+selected_background_property = {"name": "selected_background", "type": "color", "group": "Selected Style" } #, "description": "Background color when component is in the selected state."}
+selected_icon_color_property = {"name": "selected_icon_color", "type": "color", "group": "Selected Style" } #, "description": "Icon color when component is in the selected state."}
 
 class ToggleIconButton(IconButton):
   _anvil_properties_ = [selected_property, selected_border_property, selected_background_property, selected_icon_color_property, *icon_button_properties]
