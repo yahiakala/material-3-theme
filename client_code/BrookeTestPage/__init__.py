2d1d3c07bd013723333f9e4537c571e2667db26f
brooke@anvil.works <brooke@anvil.works>
2023-11-03 11:38:55 +0000

    Created BrookeTestPage; Edited settings

---
+++ client_code/BrookeTestPage/__init__.py
@@ -0,0 +1,17 @@
+from ._anvil_designer import BrookeTestPageTemplate
+from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class BrookeTestPage(BrookeTestPageTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def dropdown_menu_1_change(self, **event_args):
+    """This method is called when an item is selected"""
+    print('hello')
