1f421595f6287b9bd72d9bc4617b202e955d895c
brooke@anvil.works <brooke@anvil.works>
2023-11-06 10:46:49 +0000

    Edited Functions

--- client_code/Functions.py
+++ client_code/Functions.py
@@ -9,7 +9,7 @@ def m3_alert(title=None, content=None, buttons="OK", icon=None, dividers=False, 
 
 def theme_color_to_css(color:str):
   if color.startswith('theme:'):
-    color = color.strip('theme:')
+    color = color.lstrip('theme:')
     return app.theme_colors[color]
   else:
     return color
