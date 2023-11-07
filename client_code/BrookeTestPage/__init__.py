f01026eaae98e1c6cd5debc5a698527f8e6c9542
brooke@anvil.works <brooke@anvil.works>
2023-11-07 14:24:12 +0000

    Edited BrookeTestPage and Components.FileLoader

--- client_code/BrookeTestPage/__init__.py
+++ client_code/BrookeTestPage/__init__.py
@@ -17,9 +17,5 @@ class BrookeTestPage(BrookeTestPageTemplate):
     print(file)
     anvil.media.download(file)
 
-  def file_loader_3_change(self, **event_args):
-    """This method is called when a new file is loaded into this FileLoader"""
-    pass
-
 
 
