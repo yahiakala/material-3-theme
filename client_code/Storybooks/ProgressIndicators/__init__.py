6b12eb6535e3810dc5fb419e13be3f5073429534
char@anvil.works <char@anvil.works>
2023-11-08 14:09:45 +0000

    Edited settings and Storybooks.ProgressIndicators

--- client_code/Storybooks/ProgressIndicators/__init__.py
+++ client_code/Storybooks/ProgressIndicators/__init__.py
@@ -4,6 +4,7 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+import time
 
 class ProgressIndicators(ProgressIndicatorsTemplate):
   def __init__(self, **properties):
@@ -11,3 +12,20 @@ class ProgressIndicators(ProgressIndicatorsTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    self.circular_progress_indicator_1_copy_1.progress = 0
+    self.linear_progress_indicator_1.progress = 0
+    self.change_progress()
+    
+  def change_progress(self):
+    start_time = time.time()
+    end_time = start_time + 20  # 20 seconds timer
+
+    while time.time() < end_time and self.circular_progress_indicator_1_copy_1.progress <= 100:
+        self.circular_progress_indicator_1_copy_1.progress += 10
+        self.linear_progress_indicator_1.progress += 10
+        time.sleep(0.1)  # Adjust the sleep time to change the speed of counting
+
+    
+    
