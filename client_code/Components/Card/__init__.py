0bbcc833c0c5478551e4f46fe7954cda5256d085
char@anvil.works <char@anvil.works>
2023-11-09 14:18:29 +0000

    Edited settings, Components.Card and Storybooks.Cards

--- client_code/Components/Card/__init__.py
+++ client_code/Components/Card/__init__.py
@@ -4,28 +4,36 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from ...Functions import property_with_callback
 
 class Card(CardTemplate):
   def __init__(self, **properties):
+    self._props = properties
     self.init_components(**properties)
     self.dom_nodes['anvil-m3-card'].addEventListener("click", self.handle_click)
 
   def handle_click(self, event):
     self.raise_event("click")
 
-  @property
-  def appearance(self):
-    return self._appearance
+  def set_appearance(self, value):
+    self.dom_nodes['anvil-m3-card'].classList.toggle('anvil-m3-outlined', False)
+    self.dom_nodes['anvil-m3-card'].classList.toggle('anvil-m3-filled', False)
+    self.dom_nodes['anvil-m3-card'].classList.toggle('anvil-m3-elevated', False)
+    self.dom_nodes['anvil-m3-card'].classList.toggle(f'anvil-m3-{value}', True)
+  appearance = property_with_callback("appearance", set_appearance)
+  # @property
+  # def appearance(self):
+  #   return self._appearance
     
-  @appearance.setter
-  def appearance(self, value):
-    self._appearance = value
-    classes = self.dom_nodes['anvil-m3-card'].classList
-    classes.remove("anvil-m3-outlined")
-    classes.remove("anvil-m3-filled")
-    classes.remove("anvil-m3-elevated")
-    if (value):
-      classes.add(f"anvil-m3-{value}")
+  # @appearance.setter
+  # def appearance(self, value):
+  #   self._appearance = value
+  #   classes = self.dom_nodes['anvil-m3-card'].classList
+  #   classes.remove("anvil-m3-outlined")
+  #   classes.remove("anvil-m3-filled")
+  #   classes.remove("anvil-m3-elevated")
+  #   if (value):
+  #     classes.add(f"anvil-m3-{value}")
     
   @property
   def interactive(self):
