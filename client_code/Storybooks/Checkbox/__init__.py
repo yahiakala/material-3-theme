04f47f8924b73a68023d332935e55327cdd762fb
brooke@anvil.works <brooke@anvil.works>
2023-11-13 16:06:02 +0000

    Created Storybooks.Checkbox; Edited settings; Deleted Storybooks.Form1

--- client_code/Storybooks/Checkbox/__init__.py
+++ client_code/Storybooks/Checkbox/__init__.py
@@ -1,13 +1,15 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import CheckboxTemplate
 from anvil import *
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class Form1(Form1Template):
+class Checkbox(CheckboxTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.checkbox_3.checked = None
+    self.checkbox_5.checked = None
 
     # Any code you write here will run before the form opens.
