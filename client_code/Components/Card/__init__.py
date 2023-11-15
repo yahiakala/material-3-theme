b912bc4f2686c4df7cd6044b7032fde4c037719a
char@anvil.works <char@anvil.works>
2023-11-15 18:36:52 +0000

    Edited settings, Components.Card and anvil-m3/card.css

--- client_code/Components/Card/__init__.py
+++ client_code/Components/Card/__init__.py
@@ -4,6 +4,7 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+import anvil.designer
 
 class Card(CardTemplate):
   def __init__(self, **properties):
