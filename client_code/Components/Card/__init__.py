48adefaf02339fa9647011954d58780c6e64778c
char@anvil.works <char@anvil.works>
2023-11-15 18:48:13 +0000

    Edited Components.Card, Storybooks.Cards and anvil-m3/card.css

--- client_code/Components/Card/__init__.py
+++ client_code/Components/Card/__init__.py
@@ -5,13 +5,61 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.designer
+from anvil import HtmlTemplate
+from ...Functions import property_with_callback, style_property
 
 class Card(CardTemplate):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
+    self._props = properties
     self.init_components(**properties)
-    if anvil.designer.in_designer:
+    if not anvil.designer.in_designer:
       self.dom_nodes['empty-image'].style.display = "none"
       self.dom_nodes['empty-content'].style.display = "none"
 
-    # Any code you write here will run before the form opens.
+  align = style_property('anvil-m3-card-component', 'justifyContent', 'align')
+  visible = HtmlTemplate.visible
+
+  def set_nodes_class_by_image(self, image_position, val):
+    self.dom_nodes['anvil-m3-card'].classList.toggle(f'{image_position}-image', val)
+    self.dom_nodes['content'].classList.toggle(f'{image_position}-image', val)
+    self.dom_nodes['image'].classList.toggle(f'{image_position}-image', val)
+    
+  def set_image_position(self, value):
+    for position in ['none', 'top', 'bottom', 'left', 'right', 'full']:
+      self.set_nodes_class_by_image(position, False)
+    self.set_nodes_class_by_image(value, True)
+  image_position = property_with_callback("image_position", set_image_position)
+
+  def set_class_of_nodes(self, appearance, val):
+    self.dom_nodes['anvil-m3-card'].classList.toggle(f'anvil-m3-{appearance}', val)
+    self.dom_nodes['image'].classList.toggle(f'anvil-m3-{appearance}', val)
+    self.dom_nodes['content'].classList.toggle(f'anvil-m3-{appearance}', val)
+    
+  def set_appearance(self, value):
+    for appearance in ['outlined', 'filled', 'elevated']:
+      self.set_class_of_nodes(appearance, False)
+
+    self.set_class_of_nodes(value, True)
+  appearance = property_with_callback("appearance", set_appearance)
+
+  def set_image(self, value):
+    if value:
+      self.dom_nodes['image'].style.backgroundImage = f"url('{value}')";
+    else:
+      self.dom_nodes['image'].style.element.style.removeProperty = "background-image"
+  card_image = property_with_callback("card_image", set_image)
+
+"""
+
+<div anvil-name="anvil-m3-card-component" style-"display: flex">
+  <div anvil-name="anvil-m3-card" class="anvil-m3-card">
+    <div anvil-slot="card-image-slot" anvil-name="image" class="anvil-m3-card-image">
+      <p anvil-name="empty-image" anvil-if-slot-empty="card-image-slot" > IMG </p>
+    </div>
+    <div anvil-slot="card-content-slot" anvil-name="content" class="anvil-m3-card-content">
+      <p anvil-name="empty-content" anvil-if-slot-empty="card-content-slot" > CONT </p>
+    </div>
+  </div>
+</div>
+
+"""
