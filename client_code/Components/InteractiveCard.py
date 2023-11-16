6df8a1ef5a241d92cd3cdb8b370535a3976ee549
char@anvil.works <char@anvil.works>
2023-11-16 13:39:48 +0000

    Edited Components.InteractiveCard and anvil-m3/card.css

--- client_code/Components/InteractiveCard.py
+++ client_code/Components/InteractiveCard.py
@@ -10,14 +10,14 @@ class InteractiveCard(Card):
   _anvil_properties_ = [enabled_property, *Card._anvil_properties_]
   _anvil_events_ = [click_event, *Card._anvil_events_]
   def __init__(self, **properties):
-    self.dom_nodes['anvil-m3-card'].classList.toggle('interactive', True)
     super().__init__(**properties)
     self.init_components(**properties)
+    self.dom_nodes['anvil-m3-card'].classList.toggle('interactive', True)
     self.handle_click = self.handle_click
     self.add_event_handler("x-anvil-page-added", self.on_mount)
     self.add_event_handler("x-anvil-page-removed", self.on_cleanup)
 
-  def set_enabled(self, value):
+  def set_enabled(self, value): #why not being set in the beginning??
     self.dom_nodes['anvil-m3-card'].classList.toggle('disabled', not value)
   enabled = property_with_callback("enabled", set_enabled)
   
