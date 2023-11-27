4c072848f3a07416efcd070e1adcdb4c93d07f85
char@anvil.works <char@anvil.works>
2023-11-27 16:22:13 +0000

    Created Storybooks.InteractiveCards_1; Edited Storybooks.InteractiveCards

--- client_code/Storybooks/InteractiveCards_1/__init__.py
+++ client_code/Storybooks/InteractiveCards_1/__init__.py
@@ -1,13 +1,13 @@
-from ._anvil_designer import InteractiveCardsTemplate
+from ._anvil_designer import InteractiveCards_1Template
 from anvil import *
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from ...Components.Card import Card
 
-class InteractiveCards(InteractiveCardsTemplate):
+class InteractiveCards_1(InteractiveCards_1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
-    # Any code you write here will run before the form opens.
+    
