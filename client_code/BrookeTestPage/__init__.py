3d56b499b86b375f3807d1022e074d32c7f7a5a2
brooke@anvil.works <brooke@anvil.works>
2023-11-30 17:31:07 +0000

    Created SidesheetContent; Edited settings and BrookeTestPage

--- client_code/BrookeTestPage/__init__.py
+++ client_code/BrookeTestPage/__init__.py
@@ -6,11 +6,13 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import time
+from ..AnotherPage import AnotherPage
 
 class BrookeTestPage(BrookeTestPageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.
     
 
     # Any code you write here will run before the form opens.
