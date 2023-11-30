8f5c03b771098456fce789f6f8bdff664819751c
brooke@anvil.works <brooke@anvil.works>
2023-11-30 17:40:43 +0000

    Edited BrookeTestPage

--- client_code/BrookeTestPage/__init__.py
+++ client_code/BrookeTestPage/__init__.py
@@ -12,7 +12,6 @@ class BrookeTestPage(BrookeTestPageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.
     
 
     # Any code you write here will run before the form opens.
