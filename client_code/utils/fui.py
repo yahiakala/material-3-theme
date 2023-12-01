4f0e69e1fdc70c5bcb0c94955cfd3c0531155273
stu@anvil.works <stu@anvil.works>
2023-12-01 14:05:33 +0800

    some name tweaks

--- client_code/utils/fui.py
+++ client_code/utils/fui.py
@@ -1,15 +1,16 @@
 from anvil.js import import_from
 
+# https://floating-ui.com/
 fui = import_from("https://cdn.jsdelivr.net/npm/@floating-ui/dom@1.5.3/+esm")
 
-def auto_update(target, element, *, placement="bottom", strategy="absolute", offset=6, padding=5):
+def auto_update(reference_el, floating_el, *, placement="bottom", strategy="absolute", offset=6, padding=5):
   """starts auto updating position of element to target
   returns a cleanup function
   call this function in x-anvil-page-added
   call the cleanup in x-anvil-page-removed"""
 
   def update(*args):
-    rv = fui.computePosition(target, element, {
+    rv = fui.computePosition(reference_el, floating_el, {
       'placement': placement,
       'strategy': strategy,
       'middleware': [
@@ -19,7 +20,7 @@ def auto_update(target, element, *, placement="bottom", strategy="absolute", off
         # fui.arrow({'element': arrowElement}),
       ],
     })
-    element.style.left = f"{rv.x}px"
-    element.style.top = f"{rv.y}px"
+    floating_el.style.left = f"{rv.x}px"
+    floating_el.style.top = f"{rv.y}px"
 
-  return fui.autoUpdate(target, element, update)
+  return fui.autoUpdate(reference_el, floating_el, update)
