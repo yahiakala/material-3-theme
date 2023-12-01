b9b5f6b136c0844cb4230b476e383f8bf5390254
brooke@anvil.works <brooke@anvil.works>
2023-12-01 10:23:44 +0000

    Edited Modals

--- client_code/Modals.py
+++ client_code/Modals.py
@@ -3,5 +3,5 @@ import anvil.server
 def m3_alert(title=None, content=None, buttons="OK", icon=None, dividers=False, centered=False):
   pass
 
-def sidesheet():
+def sidesheet(content=None, dismissable=True, modal=False):
   pass
