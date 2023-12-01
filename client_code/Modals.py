7089aa2f4d0c42faad48d127daf211ea8844d5bd
brooke@anvil.works <brooke@anvil.works>
2023-12-01 14:28:55 +0000

    Edited Modals

--- client_code/Modals.py
+++ client_code/Modals.py
@@ -4,8 +4,11 @@ from anvil.js.window import document
 def m3_alert(title=None, content=None, buttons="OK", icon=None, dividers=False, centered=False):
   pass
 
-def sidesheet(content=None, dismissable=True, modal=False):
-  #need to apend a sidesheet element to the body that is removed when dissmissed or when the form changes
-  # document.body
-  pass
+Class 
+
+# def sidesheet(content=None, dismissable=True, modal=False):
+#   #need to apend a sidesheet element to the body that is removed when dissmissed or when the form changes
+  
+#   document.body.append()
+#   # pass
   
