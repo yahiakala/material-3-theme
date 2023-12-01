1871fd107d791d239ba9df8940d8dde0a2f0fa8c
brooke@anvil.works <brooke@anvil.works>
2023-12-01 10:44:22 +0000

    Edited Modals

--- client_code/Modals.py
+++ client_code/Modals.py
@@ -1,7 +1,11 @@
 import anvil.server
+from anvil.js.window import document
 
 def m3_alert(title=None, content=None, buttons="OK", icon=None, dividers=False, centered=False):
   pass
 
 def sidesheet(content=None, dismissable=True, modal=False):
-  pass
+  def _cleanup_sidesheet():
+    pass
+
+  
