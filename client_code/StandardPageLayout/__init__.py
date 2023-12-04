45bbddc7d5935311273983b9b9f28abc68825907
brooke@anvil.works <brooke@anvil.works>
2023-12-04 16:49:50 +0000

    Edited StandardPageLayout

--- client_code/StandardPageLayout/__init__.py
+++ client_code/StandardPageLayout/__init__.py
@@ -13,57 +13,57 @@ class StandardPageLayout(StandardPageLayoutTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     
-  #   self.app_bar = self.dom_nodes['anvil-m3-top-app-bar']
-  #   self.nav_drawer_open_btn = self.dom_nodes['anvil-m3-drawer-open-btn']
-  #   self.nav_drawer_close_btn = self.dom_nodes['anvil-m3-drawer-close-btn']
-  #   self.nav_rail = self.dom_nodes['anvil-m3-navigation-rail']
-  #   self.nav_drawer_scrim = self.dom_nodes['anvil-m3-navigation-drawer-scrim']
-  #   # self.sidesheet_scrim = self.dom_nodes['anvil-m3-sidesheet-scrim']
-  #   # self.sidesheet = self.dom_nodes['anvil-m3-sidesheet']
-  #   self.content = self.dom_nodes['anvil-m3-content']
+    self.app_bar = self.dom_nodes['anvil-m3-top-app-bar']
+    self.nav_drawer_open_btn = self.dom_nodes['anvil-m3-drawer-open-btn']
+    self.nav_drawer_close_btn = self.dom_nodes['anvil-m3-drawer-close-btn']
+    self.nav_rail = self.dom_nodes['anvil-m3-navigation-rail']
+    self.nav_drawer_scrim = self.dom_nodes['anvil-m3-navigation-drawer-scrim']
+    self.sidesheet_scrim = self.dom_nodes['anvil-m3-sidesheet-scrim']
+    self.sidesheet = self.dom_nodes['anvil-m3-sidesheet']
+    self.content = self.dom_nodes['anvil-m3-content']
 
-  #   window.document.addEventListener('scroll', self.add_scroll_class)
-  #   self.nav_drawer_open_btn.addEventListener('click', self.open_nav_drawer)
-  #   self.nav_drawer_close_btn.addEventListener('click', self.hide_nav_drawer)
-  #   self.nav_drawer_scrim.addEventListener('click', self.hide_nav_drawer)
+    window.document.addEventListener('scroll', self.add_scroll_class)
+    self.nav_drawer_open_btn.addEventListener('click', self.open_nav_drawer)
+    self.nav_drawer_close_btn.addEventListener('click', self.hide_nav_drawer)
+    self.nav_drawer_scrim.addEventListener('click', self.hide_nav_drawer)
   #   #self.sidesheet_scrim.addEventListener('click', self.close_sidesheet)
 
-  # def test_function(self):
-  #   print("I'm a function in StandardPageLayout")
+  def test_function(self):
+    print("I'm a function in StandardPageLayout")
 
-  # def open_nav_drawer(self, e):
-  #   self.dom_nodes['anvil-m3-navigation-rail'].style.width = '360px'
-  #   self.dom_nodes['anvil-m3-navigation-rail'].style.left = "0px"
-  #   self.dom_nodes['anvil-m3-navigation-rail'].classList.add('anvil-m3-shown')
-  #   self.nav_drawer_scrim.animate([{'opacity': '0'},{'opacity': '1'}], {'duration': 250, 'iterations': 1})
+  def open_nav_drawer(self, e):
+    self.dom_nodes['anvil-m3-navigation-rail'].style.width = '360px'
+    self.dom_nodes['anvil-m3-navigation-rail'].style.left = "0px"
+    self.dom_nodes['anvil-m3-navigation-rail'].classList.add('anvil-m3-shown')
+    self.nav_drawer_scrim.animate([{'opacity': '0'},{'opacity': '1'}], {'duration': 250, 'iterations': 1})
 
-  # def hide_nav_drawer(self, e):
-  #   self.nav_rail.style.left = "-101%"
-  #   self.nav_drawer_scrim.animate([{'opacity': '1'},{'opacity': '0'}], {'duration': 250, 'iterations': 1})
-  #   window.setTimeout(lambda: self.nav_rail.style.setProperty('width', '0px'), 250)
-  #   window.setTimeout(lambda: self.nav_rail.classList.remove('anvil-m3-shown'), 245)
+  def hide_nav_drawer(self, e):
+    self.nav_rail.style.left = "-101%"
+    self.nav_drawer_scrim.animate([{'opacity': '1'},{'opacity': '0'}], {'duration': 250, 'iterations': 1})
+    window.setTimeout(lambda: self.nav_rail.style.setProperty('width', '0px'), 250)
+    window.setTimeout(lambda: self.nav_rail.classList.remove('anvil-m3-shown'), 245)
 
         
-  # def add_scroll_class(self, e):
-  #   if self.app_bar.classList.contains('anvil-m3-scrolled'):
-  #     if window.scrollY == 0:
-  #       self.app_bar.classList.remove('anvil-m3-scrolled')
-  #   else:
-  #     self.app_bar.classList.add('anvil-m3-scrolled')    
+  def add_scroll_class(self, e):
+    if self.app_bar.classList.contains('anvil-m3-scrolled'):
+      if window.scrollY == 0:
+        self.app_bar.classList.remove('anvil-m3-scrolled')
+    else:
+      self.app_bar.classList.add('anvil-m3-scrolled')    
   
-  # @property
-  # def navigation_rail_collapse_to(self):
-  #   return self._navigation_rail_collapse_to
+  @property
+  def navigation_rail_collapse_to(self):
+    return self._navigation_rail_collapse_to
 
-  # @navigation_rail_collapse_to.setter
-  # def navigation_rail_collapse_to(self, value):
-  #   self._navigation_rail_collapse_to = value
-  #   value = value.lower().replace(' ', '-')
-  #   self.nav_rail.className = "anvil-m3-navigation-rail"
-  #   self.nav_rail.classList.add(f"anvil-m3-{value}")
-  #   #todo: remove the below?
-  #   self.content.className = "anvil-m3-content"
-  #   self.content.classList.add(f"anvil-m3-{value}")
+  @navigation_rail_collapse_to.setter
+  def navigation_rail_collapse_to(self, value):
+    self._navigation_rail_collapse_to = value
+    value = value.lower().replace(' ', '-')
+    self.dom_nodes['anvil-m3-navigation-rail'].className = "anvil-m3-navigation-rail"
+    self.dom_nodes['anvil-m3-navigation-rail'].classList.add(f"anvil-m3-{value}")
+    #todo: remove the below?
+    self.dom_nodes['anvil-m3-content'].className = "anvil-m3-content"
+    self.dom_nodes['anvil-m3-content'].classList.add(f"anvil-m3-{value}")
 
   # @property
   # def show_sidesheet(self):
@@ -74,29 +74,29 @@ class StandardPageLayout(StandardPageLayoutTemplate):
   # def show_sidesheet(self, value):
   #   self._show_sidesheet = value
   #   print('in setter:', value)
-  #   # if value:
-  #   #   self.open_sidesheet()
-  #   # else:
-  #   #   self.close_sidesheet()
+  #   if value:
+  #     self.open_sidesheet()
+  #   else:
+  #     self.close_sidesheet()
 
-  # # def open_sidesheet(self, e):
-  # #   self.sidesheet.style.width = '300px'
-  # #   self.sidesheet.style.right = "0px"
-  # #   self.sidesheet.classList.add('anvil-m3-open')
-  # #   self.sidesheet_scrim.classList.add('anvil-m3-sidesheet-open')
-  # #   self.content.classList.add('anvil-m3-sidesheet-open')
-  # #   self.sidesheet_scrim.animate([{'opacity': '0'},{'opacity': '1'}], {'duration': 250, 'iterations': 1})
+  # def open_sidesheet(self, e):
+  #   self.sidesheet.style.width = '300px'
+  #   self.sidesheet.style.right = "0px"
+  #   self.sidesheet.classList.add('anvil-m3-open')
+  #   self.sidesheet_scrim.classList.add('anvil-m3-sidesheet-open')
+  #   self.content.classList.add('anvil-m3-sidesheet-open')
+  #   self.sidesheet_scrim.animate([{'opacity': '0'},{'opacity': '1'}], {'duration': 250, 'iterations': 1})
     
-  # # def close_sidesheet(self, e):
-  # #   self.sidesheet.style.right = "-101%"
-  # #   self.sidesheet_scrim.animate([{'opacity': '1'},{'opacity': '0'}], {'duration': 250, 'iterations': 1})
-  # #   window.setTimeout(lambda: self.sidesheet.style.setProperty('width', '0px'), 250)
-  # #   window.setTimeout(lambda: self.sidesheet.classList.remove('anvil-m3-open'), 245)
-  # #   self.sidesheet_scrim.classList.remove('anvil-m3-sidesheet-open')
-  # #   self.content.classList.remove('anvil-m3-sidesheet-open')
+  # def close_sidesheet(self, e):
+  #   self.sidesheet.style.right = "-101%"
+  #   self.sidesheet_scrim.animate([{'opacity': '1'},{'opacity': '0'}], {'duration': 250, 'iterations': 1})
+  #   window.setTimeout(lambda: self.sidesheet.style.setProperty('width', '0px'), 250)
+  #   window.setTimeout(lambda: self.sidesheet.classList.remove('anvil-m3-open'), 245)
+  #   self.sidesheet_scrim.classList.remove('anvil-m3-sidesheet-open')
+  #   self.content.classList.remove('anvil-m3-sidesheet-open')
 
-  # def icon_button_1_click(self, **event_args):
-  #   self.show_sidesheet = False
+  def icon_button_1_click(self, **event_args):
+    self.show_sidesheet = False
   
       
       
