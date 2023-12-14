5d472d44b8527df951d0e70a95e41615619a4945
brooke@anvil.works <brooke@anvil.works>
2023-12-14 16:15:11 +0000

    Edited Components.RadioButton and BrookeTestPage

--- client_code/Components/RadioButton/__init__.py
+++ client_code/Components/RadioButton/__init__.py
@@ -44,7 +44,7 @@ class RadioButton(RadioButtonTemplate):
   background = color_property('anvil-m3-radiobutton-container', 'backgroundColor', 'background')
   align = style_property('anvil-m3-radiobutton-component', 'justifyContent', 'align')
   selected = checked_property('anvil-m3-radiobutton-input')
-  # margin = margin_property('anvil')
+  margin = margin_property('anvil-m3-radiobutton-component')
 
   def set_text(self, value):
     v = value
