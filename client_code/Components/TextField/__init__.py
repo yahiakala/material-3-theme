8170ff4a830f908d94a57b01627390f95094d6f4
char@anvil.works <char@anvil.works>
2023-12-28 09:40:54 +0000

    Edited Components.TextField and anvil-m3/text-fields.css

--- client_code/Components/TextField/__init__.py
+++ client_code/Components/TextField/__init__.py
@@ -61,6 +61,11 @@ class TextField(TextFieldTemplate):
   # label_text = innerText_property('label-text')
   def set_label(self, value):
     self.dom_nodes['label-text'].innerText = value or ""
+    if value:
+      self.dom_nodes['text-field-input'].classList.toggle('has_label_text', True)
+    else:
+      self.dom_nodes['text-field-input'].classList.toggle('has_label_text', anvil.designer.in_designer);
+        
   label_text = property_with_callback("label_text", set_label)
     
   italic_display = italic_property('text-field-input', 'italic_label')
