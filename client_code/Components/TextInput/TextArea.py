77d826f622eb9cc2e8c53ca122b50478e2bbf0c4
char@anvil.works <char@anvil.works>
2024-01-03 19:07:42 +0000

    Edited anvil-m3/text-inputs.css, Components.TextInput.TextArea, anvil-m3/text-fields.css and Components.TextInput

--- client_code/Components/TextInput/TextArea.py
+++ client_code/Components/TextInput/TextArea.py
@@ -4,12 +4,11 @@ import anvil.designer
 from ...Functions import property_with_callback, theme_color_to_css
 
 class TextArea(TextInput):
-  # _anvil_properties_ = [*TextInput._anvil_properties_]
   def __init__(self, **properties):
     super().__init__(**properties)
     self.init_components(**properties)
     self.dom_nodes['textarea'].classList.remove('anvil-m3-hide-tag')
-    # console.log(self)
+    # todo: add id attribute to input 
     
   def set_placeholder(self, value):
     input = self.dom_nodes['textarea']
