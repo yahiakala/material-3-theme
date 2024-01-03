303437703bc59a91b3c35a8470b0e61dc19c814c
char@anvil.works <char@anvil.works>
2024-01-03 14:29:40 +0000

    Created Components.TextInput.TextArea; Edited Storybooks.TextInputs and Components.TextInput; Deleted Components.TextInput.TextArea and Components.TextInput.TextField

---
+++ client_code/Components/TextInput/TextArea.py
@@ -0,0 +1,68 @@
+import anvil.server
+from . import TextInput
+import anvil.designer
+from ..Functions import property_with_callback, theme_color_to_css
+
+selected_property = {"name": "max_height", "type": "string", "group": "Interaction", "important": True, "default_value": False, "description": "True if button is in the toggled on state"}
+selected_border_property = {"name": "selected_border", "type": "string", "group": "Toggled Style" , "description": "Border style when component is in the selected state. Accepts any valid CSS border value."}
+selected_background_property = {"name": "selected_background", "type": "color", "group": "Toggled Style" , "description": "Background color when component is in the selected state."}
+selected_icon_color_property = {"name": "selected_icon_color", "type": "color", "group": "Toggled Style" , "description": "Icon color when component is in the selected state."}
+
+class ToggleIconButton(IconButton):
+  _anvil_properties_ = [selected_property, selected_border_property, selected_background_property, selected_icon_color_property, *IconButton._anvil_properties_]
+  def __init__(self, **properties):
+    super().__init__(**properties)
+    self.init_components(**properties)
+  
+    self.dom_nodes['anvil-m3-iconbutton-container'].classList.toggle("toggle", True)
+    self.dom_nodes['anvil-m3-iconbutton-icon'].classList.toggle("toggle", True)   
+
+    # I think there is a smarter way to do this. I don't know the setter isn't getting called in the beginning.
+    self.dom_nodes['anvil-m3-iconbutton-container'].classList.toggle("selected", self.selected)
+    self.dom_nodes['anvil-m3-iconbutton-icon'].classList.toggle("selected", self.selected)
+    
+  # Properties 
+  def apply_styles(self, value):
+    if value:
+      self.dom_nodes['anvil-m3-iconbutton-container'].style.backgroundColor = theme_color_to_css(self.selected_background) if self.selected_background else None
+      self.dom_nodes['anvil-m3-iconbutton-container'].style.border = self.selected_border
+      self.dom_nodes['anvil-m3-iconbutton-icon'].style.color = theme_color_to_css(self.selected_icon_color) if self.selected_icon_color else None
+    else:
+      self.dom_nodes['anvil-m3-iconbutton-container'].style.backgroundColor = theme_color_to_css(self.background) if self.background else None
+      self.dom_nodes['anvil-m3-iconbutton-container'].style.border = self.border
+      self.dom_nodes['anvil-m3-iconbutton-icon'].style.color = theme_color_to_css(self.icon_color) if self.icon_color else None 
+
+  def selected_setter(self, value):
+    self.dom_nodes['anvil-m3-iconbutton-container'].classList.toggle("selected", value)
+    self.dom_nodes['anvil-m3-iconbutton-icon'].classList.toggle("selected", value)
+    self.apply_styles(value)
+    
+  border = property_with_callback("border", apply_styles)
+  icon_color = property_with_callback("icon_color", apply_styles)
+  background = property_with_callback("background", apply_styles)
+  selected_border = property_with_callback("selected_border", apply_styles)
+  selected_background = property_with_callback("selected_background", apply_styles)
+  selected_icon_color = property_with_callback("selected_icon_color", apply_styles)
+  selected = property_with_callback("selected", selected_setter)
+    
+  def handle_click(self, event):
+    event.preventDefault()
+    self.selected = not self.selected
+    self.raise_event("click")
+
+  def _anvil_get_interactions_(self):
+    return [
+      {
+        "type": "region",
+        "bounds": self.dom_nodes['anvil-m3-iconbutton-component-container'],
+        "sensitivity": 0,
+        "callbacks": {
+          "execute": self.toggle_selected
+        }
+      }
+    ]
+
+  def toggle_selected(self):
+    self.selected = not self.selected
+    anvil.designer.update_component_properties(self, {'selected': self.selected})
+    
