b111bf3a48bcafd8e523331fadc5f071eabfedc2
char@anvil.works <char@anvil.works>
2024-01-03 11:09:21 +0000

    Edited Components.TextInput

--- client_code/Components/TextInput/__init__.py
+++ client_code/Components/TextInput/__init__.py
@@ -4,11 +4,20 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from ..Functions import property_without_callback
 
 class TextInput(TextInputTemplate):
+  _anvil_properties_ = [{"name": "label_text", "type": "string", "default_value": '', "group": "Attributes"},
+                        {"name": "supporting_text", "type": "string", "default_value": '', "group": "Attributes"},
+                        {"name": "placeholder", "type": "string", "default_value": '', "group": "Attributes"},]
   def __init__(self, **properties):
+    
     self._props = properties
     
     self.init_components(**properties)
+
+  label_text = property_without_callback("label_text")
+  supporting_text = property_without_callback("supporting_text")
+  placeholder = property_without_callback("label_text")
 
     # Any code you write here will run before the form opens.
