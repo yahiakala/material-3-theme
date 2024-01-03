372d7a28356b17bb541a50fec839eeed643ca839
char@anvil.works <char@anvil.works>
2024-01-03 16:29:19 +0000

    Edited anvil-m3/text-inputs.css, Storybooks.TextInputs and Components.TextInput

--- client_code/Components/TextInput/__init__.py
+++ client_code/Components/TextInput/__init__.py
@@ -9,17 +9,27 @@ from anvil import HtmlTemplate
 
 class TextInput(TextInputTemplate):
   def __init__(self, **properties):
-    
     self._props = properties
     self.init_components(**properties)
     
   visible = HtmlTemplate.visible
-  supporting_text = property_without_callback("supporting_text")
-  placeholder = property_without_callback("label_text")
-
+  # supporting_text = property_without_callback("supporting_text")
+  # placeholder = property_without_callback("label_text")
+  """
+  # background = color_property('text-field-input', 'backgroundColor', 'background')
+  italic_label = italic_property('label-text', 'italic_label')
+  bold_label = bold_property('label-text', 'bold_label')
+  underline_label = underline_property('label-text', 'underline_label')
+  label_font_size = font_size_property('label-text', 'label_font_size')
+  label_font = font_family_property('label-text', 'label_font')
+  label_text_color = color_property('label-text', 'color', 'label_text_color')
+  """
   def set_appearance(self, value):
     self.dom_nodes['textinput-component'].classList.toggle('outlined', bool(value))  
   appearance = property_with_callback("appearance", set_appearance)
+  def set_supporting_text(self, value):
+    self.dom_nodes['supporting-text'].innerHTML = value
+  supporting_text = property_with_callback("supporting_text", set_supporting_text)
 
   def form_show(self, **event_args):
     if anvil.designer.in_designer:
