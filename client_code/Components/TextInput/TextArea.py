1a92249f684a5605f73da8227c41b3dc068c6f35
char@anvil.works <char@anvil.works>
2024-01-08 16:07:35 +0000

    Edited Components.TextInput.TextArea

--- client_code/Components/TextInput/TextArea.py
+++ client_code/Components/TextInput/TextArea.py
@@ -1,6 +1,8 @@
 import anvil.server
 from . import TextInput
 import anvil.designer
+import anvil.js
+from anvil.js.window import ResizeObserver
 from ...Functions import property_with_callback, theme_color_to_css
 
 class TextArea(TextInput):
@@ -9,6 +11,7 @@ class TextArea(TextInput):
     self.init_components(**properties)
     hiddenInput = self.dom_nodes['textfield']
     self.dom_nodes['input-container'].removeChild(hiddenInput)
+    
     
   def set_placeholder(self, value):
     input = self.dom_nodes['textarea']
@@ -41,6 +44,65 @@ class TextArea(TextInput):
   def set_id(self, value):
     super().set_id(value)
     self.dom_nodes["textarea"].id = value
+
+"""
+
+const h1Elem = document.querySelector("h1");
+const pElem = document.querySelector("p");
+const divElem = document.querySelector("body > div");
+const slider = document.querySelector('input[type="range"]');
+const checkbox = document.querySelector('input[type="checkbox"]');
+
+divElem.style.width = "600px";
+
+slider.addEventListener("input", () => {
+  divElem.style.width = `${slider.value}px`;
+});
+
+const resizeObserver = new ResizeObserver((entries) => {
+  for (const entry of entries) {
+    if (entry.contentBoxSize) {
+      const contentBoxSize = entry.contentBoxSize[0];
+      h1Elem.style.fontSize = `${Math.max(
+        1.5,
+        contentBoxSize.inlineSize / 200,
+      )}rem`;
+      pElem.style.fontSize = `${Math.max(
+        1,
+        contentBoxSize.inlineSize / 600,
+      )}rem`;
+    } else {
+      h1Elem.style.fontSize = `${Math.max(
+        1.5,
+        entry.contentRect.width / 200,
+      )}rem`;
+      pElem.style.fontSize = `${Math.max(1, entry.contentRect.width / 600)}rem`;
+    }
+  }
+
+  console.log("Size changed");
+});
+
+resizeObserver.observe(divElem);
+
+checkbox.addEventListener("change", () => {
+  if (checkbox.checked) {
+    resizeObserver.observe(divElem);
+  } else {
+    resizeObserver.unobserve(divElem);
+  }
+});
+
+
+self.handle_click = self.handle_click
+    self.add_event_handler("x-anvil-page-added", self.on_mount)
+    self.add_event_handler("x-anvil-page-removed", self.on_cleanup)
+
+  def on_mount(self, **event_args):
+    self.dom_nodes['anvil-m3-button'].addEventListener("click", self.handle_click)
+  def on_cleanup(self, **event_args):
+    self.dom_nodes['anvil-m3-button'].removeEventListener("click", self.handle_click)
+"""
 
 """
 <script type="text/javascript">
