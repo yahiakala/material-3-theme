e757208fa8502c28851e80d68c157af4d186da0d
char@anvil.works <char@anvil.works>
2024-01-11 10:25:36 +0000

    Created Components.DropdownMenu_deprecated and Storybooks.Dropdowns_old; Edited settings and Components.DropdownMenu; Deleted Storybooks.Dropdowns_copy and Components.DropdownMenu_copy

--- client_code/Components/DropdownMenu/__init__.py
+++ client_code/Components/DropdownMenu/__init__.py
@@ -5,7 +5,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 from anvil import HtmlTemplate
-from ...Functions import property_with_callback, property_without_callback 
+from ...Functions import property_with_callback, property_without_callback
 from anvil.js import window, get_dom_node
 from anvil.js.window import document
 import random, string, math
@@ -22,32 +22,32 @@ class DropdownMenu(DropdownMenuTemplate):
 
     self.menuNode = self.dom_nodes['anvil-m3-dropdownMenu-items-container']
     self.field = get_dom_node(self.selection_field).querySelector("input")
-    
+
     self._hoverIndex = None
     self._children = None
     self._selected_menuItem = None
-    
+
     self.selected_value = None
-    
+
     self._has_focus = False
     self.handle_keyboard_events = self.handle_keyboard_events
     self.handle_selection_field_focus = self.handle_selection_field_focus
     self.child_clicked = self.child_clicked
     self.handle_component_click = self.handle_component_click
-    
+
     self.add_event_handler("x-anvil-page-added", self.on_mount)
     self.add_event_handler("x-anvil-page-removed", self.on_cleanup)
-    
-    self.selection_field.dom_nodes['text-field-input'].style.caretColor = 'transparent'
-    self.selection_field.dom_nodes['text-field-input'].style.cursor = "pointer"
-    self.selection_field.dom_nodes['text-field-input'].setAttribute("readonly", True)
-          
-    if not self.label_text and self.placeholder:
-        self.selection_field.dom_nodes['label-text'].innerText = self.placeholder
 
-    if anvil.designer.in_designer: #hides so doesn't do the ghosty visible thing when in designer cuz i want it to just straight up not show cuz its nto like you can add stuffin anyways. 
+    self.selection_field.dom_nodes['textfield'].style.caretColor = 'transparent'
+    self.selection_field.dom_nodes['textfield'].style.cursor = "pointer"
+    self.selection_field.dom_nodes['textfield'].setAttribute("readonly", True)
+
+    if not self.label_text and self.placeholder:
+      self.selection_field.dom_nodes['label-text'].innerText = self.placeholder
+
+    if anvil.designer.in_designer: #hides so doesn't do the ghosty visible thing when in designer cuz i want it to just straight up not show cuz its nto like you can add stuffin anyways.
       self.menuNode.classList.toggle("anvil-m3-menu-hidden", True)
-    
+
   #properties
   visible = HtmlTemplate.visible
 
@@ -62,15 +62,15 @@ class DropdownMenu(DropdownMenuTemplate):
   def set_label_font(self, value):
     self.selection_field.label_font = value
   label_font = property_with_callback("label_font", set_label_font)
-  
+
   def set_label_font_size(self, value):
     self.selection_field.label_font_size = value
   label_font_size = property_with_callback("label_font_size", set_label_font_size)
-  
+
   def set_label_text_color(self, value):
     self.selection_field.label_text_color = value
   label_text_color = property_with_callback("label_text_color", set_label_text_color)
-  
+
   def set_italic_label(self, value):
     self.selection_field.italic_label = value
   italic_label = property_with_callback("italic_label", set_italic_label)
@@ -122,9 +122,9 @@ class DropdownMenu(DropdownMenuTemplate):
 
   def set_selected_value(self, value):
     if type(value) is list:
-      self.selection_field.dom_nodes['text-field-input'].value = value[0]
+      self.selection_field.dom_nodes['textfield'].value = value[0]
     else:
-      self.selection_field.dom_nodes['text-field-input'].value = value
+      self.selection_field.dom_nodes['textfield'].value = value
     self.raise_event("change")
   selected_value = property_with_callback("selected_value", set_selected_value)
 
@@ -138,35 +138,35 @@ class DropdownMenu(DropdownMenuTemplate):
   items_background = property_without_callback("items_background")
   items_font = property_without_callback("items_font")
   items_font_size = property_without_callback("items_font_size")
-    
+
   def on_mount(self, **event_args):
     document.addEventListener('keydown', self.handle_keyboard_events)
     document.addEventListener('click', self.body_click)
-    
+
     document.body.append(self.menuNode)
-    
+
     self._cleanup = fui.auto_update(self.field, self.menuNode, placement="bottom-start")
-    
+
     self.dom_nodes['anvil-m3-dropdownMenu-container'].addEventListener('click', self.handle_component_click)
-    self.selection_field.dom_nodes['text-field-input'].addEventListener('focus', self.handle_selection_field_focus)
-    self.selection_field.dom_nodes['text-field-input'].addEventListener('blur', self.handle_selection_field_blur)
+    self.selection_field.dom_nodes['textfield'].addEventListener('focus', self.handle_selection_field_focus)
+    self.selection_field.dom_nodes['textfield'].addEventListener('blur', self.handle_selection_field_blur)
     self.menuNode.addEventListener('click', self.child_clicked)
-    
+
   def on_cleanup(self, **event_args):
     document.removeEventListener('keydown', self.handle_keyboard_events)
     self.dom_nodes['anvil-m3-dropdownMenu-container'].removeEventListener('click', self.handle_component_click)
-    self.selection_field.dom_nodes['text-field-input'].removeEventListener('focus', self.handle_selection_field_focus)
-    self.selection_field.dom_nodes['text-field-input'].removeEventListener('blur', self.handle_selection_field_blur)
+    self.selection_field.dom_nodes['textfield'].removeEventListener('focus', self.handle_selection_field_focus)
+    self.selection_field.dom_nodes['textfield'].removeEventListener('blur', self.handle_selection_field_blur)
     self.menuNode.removeEventListener('click', self.child_clicked)
     self._cleanup()
     self.menuNode.remove()
-    
+
   def handle_selection_field_focus(self, event):
     self._has_focus = True
-    
+
   def handle_selection_field_blur(self, event):
     self._has_focus = False
-    
+
   def handle_keyboard_events(self, event):
     if not self._has_focus:
       return
@@ -175,13 +175,13 @@ class DropdownMenu(DropdownMenuTemplate):
       open_keys = set(["ArrowUp", "ArrowDown", " ", "Enter"])
       if not self.menu.visible:
         if event.key in open_keys:
-          self.set_menu_visibility(True)  
+          self.set_menu_visibility(True)
           event.preventDefault()
         return
-        
+
       if event.key not in action_keys: #   #TODO: eventually want to use this to jump somewhere in the list
         return
-      
+
       if event.key is "ArrowUp" or event.key is "ArrowDown":
         event.preventDefault()
         self.iterate_hover(event.key is "ArrowDown")
@@ -195,7 +195,7 @@ class DropdownMenu(DropdownMenuTemplate):
         self.attempt_select()
       if (event.key is "Enter"):
         self.attempt_select()
-    
+
   def iterate_hover(self, inc = True):
     if inc:
       if self._hoverIndex is None or self._hoverIndex is (len(self._children) - 1):
@@ -206,12 +206,12 @@ class DropdownMenu(DropdownMenuTemplate):
         self._hoverIndex = len(self._children)
       self._hoverIndex -= 1
     self.update_hover_styles()
-    
+
   def attempt_select(self):
     if not self._hoverIndex is None:
       self._children[self._hoverIndex].raise_event("click")
     self.set_menu_visibility(False)
-    
+
   def clear_hover_styles(self):
     if self._children is not None:
       for child in self._children:
@@ -223,7 +223,7 @@ class DropdownMenu(DropdownMenuTemplate):
     if self._hoverIndex is None:
       return
     self._children[self._hoverIndex].dom_nodes['anvil-m3-menuItem-container'].classList.toggle('anvil-m3-menuItem-container-keyboardHover', True)
-    
+
   def handle_component_click(self, event):
     self.set_menu_visibility()
 
@@ -247,7 +247,7 @@ class DropdownMenu(DropdownMenuTemplate):
     if self.field.contains(event.target) or self.menuNode.contains(event.target):
       return
     self.set_menu_visibility(False)
-      
+
   def child_clicked(self, event):
     event.stopPropagation()
     self.set_menu_visibility(False)
@@ -256,16 +256,16 @@ class DropdownMenu(DropdownMenuTemplate):
     else:
       self._hoverIndex = self._children.index(self._selected_menuItem)
     self.update_hover_styles()
-      
+
   def form_show(self, **event_args):
     self.create_menu_items()
     self._children = self.menu.get_components()
-    
+
     if anvil.designer.in_designer:
       self._design_name = anvil.designer.get_design_name(self)
       if not self.label_text:
         self.selection_field.dom_nodes['label-text'].innerText = self._design_name
-        
+
   def create_menu_items(self):
     p = MenuItem()
     p.text = self.placeholder if self.placeholder else "Clear Selection"
@@ -276,21 +276,21 @@ class DropdownMenu(DropdownMenuTemplate):
     p.font = self.items_font
     p.font_size = self.items_font_size
     p.hide_leading_icon = True
-    
+
     def handle_select_placeholder(**e):
       if self.allow_none: self.selected_value = None
 
     if not self.allow_none:
       p.enabled = False
-    
+
     if self.allow_none or self.placeholder:
       p.add_event_handler('click', handle_select_placeholder)
       self.menu.add_component(p, slot="anvil-m3-menu-slot")
-    
+
     for item in self.items:
       selection = MenuItem()
       selection.hide_leading_icon = True
-      
+
       selection.bold = self.bold_items
       selection.italic = self.italic_items
       selection.underline = self.underline_items
@@ -298,19 +298,19 @@ class DropdownMenu(DropdownMenuTemplate):
       selection.background = self.items_background
       selection.font = self.items_font
       selection.font_size = self.items_font_size
-      
+
       if type(item) is list:
         selection.text = item[0]
       else:
         selection.text = item
-        
-      def handle_selection_click(value = item, menuItem = selection, **e): 
+
+      def handle_selection_click(value = item, menuItem = selection, **e):
         self.selected_value = value
         self._selected_menuItem = menuItem
-          
+
       selection.add_event_handler('click', handle_selection_click)
       self.menu.add_component(selection, slot="anvil-m3-menu-slot")
- 
+
 # DESIGNER INTERACTIONS
   def _anvil_get_interactions_(self):
     return [
--- client_code/Storybooks/Dropdowns_old/__init__.py
+++ client_code/Storybooks/Dropdowns_old/__init__.py
@@ -1,11 +1,11 @@
-from ._anvil_designer import Dropdowns_copyTemplate
+from ._anvil_designer import Dropdowns_oldTemplate
 from anvil import *
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class Dropdowns_copy(Dropdowns_copyTemplate):
+class Dropdowns_old(Dropdowns_oldTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
