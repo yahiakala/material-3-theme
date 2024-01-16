487d244b615d3104de6d7a895a34a558e2b8d82e
patricia@anvil.works <patricia@anvil.works>
2024-01-16 13:39:58 +0000

    Edited Components.Slider and Storybooks.Sliders

--- client_code/Components/Slider/__init__.py
+++ client_code/Components/Slider/__init__.py
@@ -73,17 +73,16 @@ class Slider(SliderTemplate):
   def _do_hide_label(self):
     self.label_container.remove()
 
-  progress_color = color_property("anvil-m3-slider-progress", 'background', 'progress_color')
-  track_color = color_property("anvil-m3-slider-background", 'background', 'track_color')
-
-
   def _set_thumb_color(self, value=None):
     if self.thumb_color:
       document.body.style.setProperty('--anvil-m3-slider-thumb-color', theme_color_to_css(self.thumb_color))
     else:
       document.body.style.setProperty('--anvil-m3-slider-thumb-color', 'var(--anvil-m3-primary)')
-
+      
   thumb_color = property_with_callback('thumb_color', _set_thumb_color)
+  progress_color = color_property("anvil-m3-slider-progress", 'background', 'progress_color')
+  track_color = color_property("anvil-m3-slider-background", 'background', 'track_color')
+  
 
   @property
   def value(self):
