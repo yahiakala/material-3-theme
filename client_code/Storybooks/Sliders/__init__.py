36358c143f99c6f6e8b061ab882dafc163cf0a46
brooke@anvil.works <brooke@anvil.works>
2024-01-24 10:32:56 +0000

    Edited Storybooks.Sliders

--- client_code/Storybooks/Sliders/__init__.py
+++ client_code/Storybooks/Sliders/__init__.py
@@ -12,3 +12,7 @@ class Sliders(SlidersTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def slider_4_change(self, **event_args):
+    """This method is called when the value of the component is changed"""
+    print(value)
