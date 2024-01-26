7070424d6bb8f8ef952ccede8e85315b3bc5236e
patricia@anvil.works <patricia@anvil.works>
2024-01-26 11:11:23 +0000

    Created Storybooks.TextInputs_copy; Edited PatTestPage and settings

---
+++ client_code/Storybooks/TextInputs_copy/__init__.py
@@ -0,0 +1,19 @@
+from ._anvil_designer import TextInputs_copyTemplate
+from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class TextInputs_copy(TextInputs_copyTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def text_field_1_copy_2_click(self, **event_args):
+    alert("clicked outlined style")
+
+  def text_field_1_click(self, **event_args):
+    alert("clicked filled style")
