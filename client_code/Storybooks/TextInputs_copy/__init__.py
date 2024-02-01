6a11f6a99eba6691dd9f89e5890f05dbce63250f
patricia@anvil.works <patricia@anvil.works>
2024-02-01 14:38:49 +0000

    Edited Storybooks.TextInputs_copy

--- client_code/Storybooks/TextInputs_copy/__init__.py
+++ client_code/Storybooks/TextInputs_copy/__init__.py
@@ -9,6 +9,7 @@ class TextInputs_copy(TextInputs_copyTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    
 
     # Any code you write here will run before the form opens.
 
@@ -16,14 +17,19 @@ class TextInputs_copy(TextInputs_copyTemplate):
     alert("clicked outlined style")
 
   def text_field_1_click(self, **event_args):
-    alert("clicked filled style")
+    self.text_field_1.hide_text = not self.text_field_1.hide_text 
 
   def button_1_click(self, **event_args):
     """This method is called when the component is clicked."""
     self.text_field_3_copy.hide_text =  not self.text_field_3_copy.hide_text
+    
 
   def text_field_3_copy_pressed_enter(self, **event_args):
     """This method is called when the user presses enter in this component."""
     alert("Done")
 
-  
+  def update_tf1_icon(self):
+    if self.text_field_3_copy.hide_text == True:
+      self.text_field_1.trailing_icon = 'visibility'
+    else:
+      self.text_field_1.trailing_icon = 'visibility_off'
