b4a87ac9a8d4b8353b5e55811cbcc64311ccf818
brooke@anvil.works <brooke@anvil.works>
2024-02-01 18:31:06 +0000

    Edited theme.css, TestLayout and BrookeTestPage

--- client_code/BrookeTestPage/__init__.py
+++ client_code/BrookeTestPage/__init__.py
@@ -12,7 +12,7 @@ class BrookeTestPage(BrookeTestPageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.layout.layout.show_sidesheet = False
+    #self.layout.layout.show_sidesheet = False
 
 
 
