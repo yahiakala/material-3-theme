ffc3bda36338b975b4464de82e6eead194b31ac6
brooke@anvil.works <brooke@anvil.works>
2024-02-02 10:58:34 +0000

    Edited BrookeTestPage.RowTemplate2 and BrookeTestPage

--- client_code/BrookeTestPage/RowTemplate2/__init__.py
+++ client_code/BrookeTestPage/RowTemplate2/__init__.py
@@ -10,4 +10,5 @@ class RowTemplate2(RowTemplate2Template):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
+
     # Any code you write here will run before the form opens.
