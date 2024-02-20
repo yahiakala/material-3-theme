e0d7ce7f3371df067600e6c098e31a759c7e2738
patricia@anvil.works <patricia@anvil.works>
2024-02-20 10:49:22 +0000

    Edited Storybooks.TextInputs_copy

--- client_code/Storybooks/TextInputs_copy/__init__.py
+++ client_code/Storybooks/TextInputs_copy/__init__.py
@@ -21,15 +21,7 @@ class TextInputs_copy(TextInputs_copyTemplate):
   def text_field_1_click(self, **event_args):
     self.text_field_1.hide_text = not self.text_field_1.hide_text 
     self.update_tf1_icon()
-
-  def button_1_click(self, **event_args):
-    """This method is called when the component is clicked."""
-    self.text_field_3_copy.hide_text =  not self.text_field_3_copy.hide_text
     
-
-  def text_field_3_copy_pressed_enter(self, **event_args):
-    """This method is called when the user presses enter in this component."""
-    alert("Done")
 
   def update_tf1_icon(self):
     if self.text_field_1.hide_text == True:
