36e9f36a78ea7de1291b1dde3bdce67b26ea3a38
brooke@anvil.works <brooke@anvil.works>
2024-05-31 12:43:19 +0000

    Edited Components.Card

--- client_code/Components/Card/__init__.py
+++ client_code/Components/Card/__init__.py
@@ -48,8 +48,8 @@ class Card(CardTemplate):
     self._set_class_of_nodes(value, True)
   appearance = property_with_callback("appearance", _set_appearance)
 
-  image_width = align = style_property('image', 'width', 'image_width')
-  image_height = align = style_property('image', 'height', 'image_height')
+  image_width = style_property('image', 'width', 'image_width')
+  image_height = style_property('image', 'height', 'image_height')
   background = color_property('anvil-m3-card', 'backgroundColor', 'background')
 
   def _set_card_img(self, *_args):
