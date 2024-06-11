9bc53c1ea79119265dbdeedc48b89a03bf942625
brooke@anvil.works <brooke@anvil.works>
2024-06-11 13:21:08 +0000

    Created TestNavigationDrawerLayout; Edited NavigationDrawerLayout, NavigationRailLayout, TestLayout, anvil-m3/navigation-link.css and 1 more

--- client_code/NavigationDrawerLayout/__init__.py
+++ client_code/NavigationDrawerLayout/__init__.py
@@ -4,12 +4,18 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from anvil.js import window
 
 
 class NavigationDrawerLayout(NavigationDrawerLayoutTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.nav_drawer_open_btn = self.dom_nodes['anvil-m3-drawer-open-btn']
+    self.nav_drawer_close_btn = self.dom_nodes['anvil-m3-drawer-close-btn']
+    self.nav_drawer_scrim = self.dom_nodes['anvil-m3-navigation-drawer-scrim']
+
+    window.document.addEventListener('scroll', self.add_scroll_class)
     self.nav_drawer_open_btn.addEventListener('click', self.open_nav_drawer)
     self.nav_drawer_close_btn.addEventListener('click', self.hide_nav_drawer)
     self.nav_drawer_scrim.addEventListener('click', self.hide_nav_drawer)
@@ -26,3 +32,10 @@ class NavigationDrawerLayout(NavigationDrawerLayoutTemplate):
     self.nav_drawer_scrim.animate([{'opacity': '1'},{'opacity': '0'}], {'duration': 250, 'iterations': 1})
     window.setTimeout(lambda: self.nav_rail.style.setProperty('width', '0px'), 250)
     window.setTimeout(lambda: self.nav_rail.classList.remove('anvil-m3-shown'), 245)
+
+  def add_scroll_class(self, e):
+    if self.app_bar.classList.contains('anvil-m3-scrolled'):
+      if window.scrollY == 0:
+        self.app_bar.classList.remove('anvil-m3-scrolled')
+    else:
+      self.app_bar.classList.add('anvil-m3-scrolled')   
