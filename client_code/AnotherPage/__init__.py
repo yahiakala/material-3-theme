a7e702a056fa7db4f6675ec13cf55592d290b759
brooke@anvil.works <brooke@anvil.works>
2024-06-25 14:53:41 +0000

    Edited AnotherPage

--- client_code/AnotherPage/__init__.py
+++ client_code/AnotherPage/__init__.py
@@ -9,6 +9,7 @@ class AnotherPage(AnotherPageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    print(self.button_menu_1.get_components())
   
 
     # Any code you write here will run before the form opens.
