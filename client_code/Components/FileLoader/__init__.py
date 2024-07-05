560bfed8469a2d3bf182b697a3b347ebade7bf9d
brooke@anvil.works <brooke@anvil.works>
2024-07-05 10:29:58 +0000

    Edited Components.FileLoader

--- client_code/Components/FileLoader/__init__.py
+++ client_code/Components/FileLoader/__init__.py
@@ -30,7 +30,8 @@ class FileLoader(FileLoaderTemplate):
       self.dom_nodes["anvil-m3-fileloader-label"].setAttribute("for", id)
 
   def clear(self):
-    self.dom_nodes['anvil-m3-fileloader-input'].files = None
+    self.dom_nodes['anvil-m3-fileloader-input'].value = ''
+    self.file = self.dom_nodes['anvil-m3-fileloader-input'].files
     self.dom_nodes['anvil-m3-fileloader-label'].innerText = self.text
     #update show_state text if present
 
