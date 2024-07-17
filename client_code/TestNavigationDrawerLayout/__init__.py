1506582989bc84a8e5a41f1360cc5420502c8347
brooke@anvil.works <brooke@anvil.works>
2024-07-17 14:14:28 +0000

    Edited TestNavigationDrawerLayout

--- client_code/TestNavigationDrawerLayout/__init__.py
+++ client_code/TestNavigationDrawerLayout/__init__.py
@@ -14,7 +14,7 @@ class TestNavigationDrawerLayout(TestNavigationDrawerLayoutTemplate):
     # Any code you write here will run before the form opens.
 
   def icon_button_1_click(self, **event_args):
-    open_form('BrookeTestPage')
+    self.layout.layout.close_sidesheet()
 
   def button_1_click(self, **event_args):
     """This method is called when the component is clicked."""
