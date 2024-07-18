d20e731435dcfc8f5c8783b0d35349eb242c8e11
brooke@anvil.works <brooke@anvil.works>
2024-07-18 10:40:32 +0000

    Edited AnotherPage

--- client_code/AnotherPage/__init__.py
+++ client_code/AnotherPage/__init__.py
@@ -13,10 +13,20 @@ class AnotherPage(AnotherPageTemplate):
     link_1 = M3_Link(text="link", bold=True)
     link_2 = M3_Link(text="another link")
     self.button_menu_1.menu_items = [link_1, link_2]
+    self.dropdown_menu_1.items = [('Hello', True), ('Goodbye', False)]
+    self.drop_down_1.items = [('Hello', True), ('Goodbye', False)]
 
 
     # Notification("This is a notification", timeout=None, title="Notification!", style="info").show()
     # Notification("This is a notification", timeout=None, title="Notification!", style="success").show()
     # Notification("This is a notification", timeout=None, title="Notification!", style="danger").show()
     # Notification("This is a notification", timeout=None, title="Notification!", style="warning").show()
+
+  def dropdown_menu_1_change(self, **event_args):
+    """This method is called when an item is selected"""
+    print(self.dropdown_menu_1.selected_value)
+
+  def drop_down_1_change(self, **event_args):
+    """This method is called when an item is selected"""
+    print(self.drop_down_1.selected_value)
 
