969fcd736070d8fcd1cd53c52aca84237cde33c1
brooke@anvil.works <brooke@anvil.works>
2024-07-18 10:29:00 +0000

    Edited settings, AnotherPage and Components.ButtonMenu

--- client_code/AnotherPage/__init__.py
+++ client_code/AnotherPage/__init__.py
@@ -4,11 +4,15 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from ..Components.Link import Link as M3_Link
 
 class AnotherPage(AnotherPageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    link_1 = M3_Link(text="link", bold=True)
+    link_2 = M3_Link(text="another link")
+    self.button_menu_1.menu_items = [link_1, link_2]
 
 
     # Notification("This is a notification", timeout=None, title="Notification!", style="info").show()
