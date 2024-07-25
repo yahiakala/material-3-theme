85d4e8e745ff50ac1b8502b2fd4eff54713d83f4
brooke@anvil.works <brooke@anvil.works>
2024-07-25 13:15:26 +0000

    Edited AnotherPage

--- client_code/AnotherPage/__init__.py
+++ client_code/AnotherPage/__init__.py
@@ -11,17 +11,10 @@ class AnotherPage(AnotherPageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    link_1 = M3_Link(text="link", bold=True)
-    link_2 = M3_Link(text="another link")
-    self.button_menu_1.menu_items = [link_1, link_2]
-    self.dropdown_menu_1.items = [('Hello', True), ('Goodbye', False)]
-    self.drop_down_1.items = [('Hello', True), ('Goodbye', False)]
 
-
-    # Notification("This is a notification", timeout=None, title="Notification!", style="info").show()
-    # Notification("This is a notification", timeout=None, title="Notification!", style="success").show()
-    # Notification("This is a notification", timeout=None, title="Notification!", style="danger").show()
-    # Notification("This is a notification", timeout=None, title="Notification!", style="warning").show()
+  def slider_1_change(self, **event_args):
+    """This method is called when the value of the component is changed"""
+    self.text_1.text = self.slider_1.value
 
 
 
