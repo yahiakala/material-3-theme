eebdb018921057c522b23f7e0a57ed892649d7db
patricia@anvil.works <patricia@anvil.works>
2024-07-25 11:47:07 +0000

    Edited Components.Text

--- client_code/Components/Text/__init__.py
+++ client_code/Components/Text/__init__.py
@@ -39,8 +39,10 @@ class Text(TextTemplate):
   #!componentEvent(material_3.Text)!1: {name: "show", description: "When the Text is shown on the screen."}
   #!componentEvent(material_3.Text)!1: {name: "hide", description: "When the Text is removed from the screen."}
 
-  #!componentProp(material_3.Text)!1: {name:"visible",type:"boolean",description:"If True, the component will be displayed."} 
+  #!componentProp(material_3.Text)!1: {name:"visible",type:"boolean",description:"If True, the component will be displayed."}
+  #!componentProp(material_3.Text)!1: {name:"underline",type:"boolean",description:"If True, this component’s text will be underlined."}
   #!componentProp(material_3.Text)!1: {name:"italic",type:"boolean",description:"If True, this component’s text will be italic."}
+  #!componentProp(material_3.Text)!1: {name:"bold",type:"boolean",description:"If True, this component’s text will be bold."}
   #!componentProp(material_3.Text)!1: {name:"border",type:"string",description:"The border of this component. Can take any valid CSS border value."}
   #!componentProp(material_3.Text)!1: {name:"font_family",type:"string",description:"The font family to use for this component."}
   #!componentProp(material_3.Text)!1: {name:"text_color",type:"color",description:"The color of the text on the component."} 
@@ -51,8 +53,6 @@ class Text(TextTemplate):
   #!componentProp(material_3.Text)!1: {name:"align",type:"enum",options:['left', 'right', 'center', 'justify'], description:"The position of this component in the available space."} 
   #!componentProp(material_3.Text)!1: {name:"font_size",type:"number",description:"The font size of text displayed on this component."}
   #!componentProp(material_3.Text)!1: {name:"icon_size",type:"number",description:"The size (pixels) of the icon displayed on this component."}
-  #!componentProp(material_3.Text)!1: {name:"underline",type:"boolean",description:"If True, this component’s text will be underlined."}
-  #!componentProp(material_3.Text)!1: {name:"bold",type:"boolean",description:"If True, this component’s text will be bold."}
   #!componentProp(material_3.Text)!1: {name:"text",type:"string",description:"The text displayed on this component"}
   #!componentProp(material_3.Text)!1: {name:"material_icon",type:"enum",description:"The icon to display on this component."} 
   #!componentProp(material_3.Text)!1: {name:"style",type:"enum",options:['display', 'headline', 'title'],description:"Role of the text component: display, headline or title."}  
