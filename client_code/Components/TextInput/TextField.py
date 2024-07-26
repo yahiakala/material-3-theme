e425d6a04a620745fa625493b12a05f5b2a0b86d
brooke@anvil.works <brooke@anvil.works>
2024-07-26 10:03:03 +0000

    Edited AnotherPage and Components.TextInput.TextField

--- client_code/Components/TextInput/TextField.py
+++ client_code/Components/TextInput/TextField.py
@@ -291,9 +291,9 @@ class TextField(TextInput):
   #!componentProp(material_3.TextField)!1: {name:"placeholder",type:"string",description:"The placeholder text to display on this component."}
 
   #!componentProp(material_3.TextField)!1: {name:"spacing",type:"spacing",description:"The margin and padding (pixels) of the component."}
-  #!componentProp(material_3.TextField)!1: {name:"type",type:"enum",options:[],description:"The border of this component. Can take any valid CSS border value."}
+  #!componentProp(material_3.TextField)!1: {name:"type",type:"enum",options:["text", "number", "email", "tel", "url"],description:"The border of this component. Can take any valid CSS border value."}
   #!componentProp(material_3.TextField)!1: {name:"tooltip",type:"string",description:"The text to display when the mouse is hovered over this component."}
-  #!componentProp(material_3.TextField)!1: {name:"text",type:"string",description:"The text to display when the mouse is hovered over this component."}
+  #!componentProp(material_3.TextField)!1: {name:"text",type:"string",description:"The text to display on this component"}
   #!componentProp(material_3.TextField)!1: {name:"hide_text",type:"boolean",description:"The text to display when the mouse is hovered over this component."}
 
 #!defClass(material_3,TextField, anvil.Component)!:
