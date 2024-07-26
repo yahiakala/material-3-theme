432c90a2bfa9b2a522a7d04713a795546f553075
brooke@anvil.works <brooke@anvil.works>
2024-07-26 09:35:10 +0000

    Edited settings and Storybooks.Dropdowns

--- client_code/Storybooks/Dropdowns/__init__.py
+++ client_code/Storybooks/Dropdowns/__init__.py
@@ -10,4 +10,5 @@ class Dropdowns(DropdownsTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
+
     # Any code you write here will run before the form opens.
