2880fd49a98a2c81e12689fe0e826547e49e0a76
brooke@anvil.works <brooke@anvil.works>
2024-08-01 15:44:12 +0000

    Edited settings and Components.DropdownMenu

--- client_code/Components/DropdownMenu/__init__.py
+++ client_code/Components/DropdownMenu/__init__.py
@@ -282,10 +282,13 @@ class DropdownMenu(DropdownMenuTemplate):
     self._set_menu_visibility()
 
   def _set_menu_visibility(self, value = None):
+    print(value)
     if (value is None):
+      print(self.menu.visible)
       value = not self.menu.visible
     self.menu.visible = value
     if value:
+      print('yes')
       if not anvil.designer.in_designer:
         self.selection_field.trailing_icon = "arrow_drop_up"
     else:
@@ -294,6 +297,7 @@ class DropdownMenu(DropdownMenuTemplate):
         self._hoverIndex = None
 
   def _body_click(self, event):
+    print('clicked')
     if self._field.contains(event.target) or self._menuNode.contains(event.target):
       return
     self._set_menu_visibility(False)
@@ -431,3 +435,7 @@ class DropdownMenu(DropdownMenuTemplate):
   #!componentEvent(material_3.DropdownMenu)!1: {name: "change", description: "When an item is selected.", parameters:[]}
 
 #!defClass(material_3,DropdownMenu, anvil.Component)!:
+
+  def selection_field_trailing_icon_click(self, **event_args):
+    """This method is called when the trailing icon is clicked."""
+    print(event_args)
