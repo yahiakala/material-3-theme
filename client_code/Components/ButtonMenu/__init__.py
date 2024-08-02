7f1e4d69b55467d6720d5018555b1b64814a32a1
brooke@anvil.works <brooke@anvil.works>
2024-08-02 16:10:55 +0000

    Edited Components.Button and Components.ButtonMenu

--- client_code/Components/ButtonMenu/__init__.py
+++ client_code/Components/ButtonMenu/__init__.py
@@ -268,27 +268,28 @@ class ButtonMenu(ButtonMenuTemplate):
         self.menu_button.text = self._design_name
 
   #!componentProp(material_3.ButtonMenu)!1: {name:"align",type:"enum",options:["left", "right", "center"],description:"The position of this component in the available space."} 
-  #!componentProp(material_3.ButtonMenu)!1: {name:"appearance",type:"enum",options:["filled", "elevated", "tonal", "outlined", "text"],description:"A predefined style for this component."}  
+  #!componentProp(material_3.ButtonMenu)!1: {name:"appearance",type:"enum",options:["filled", "elevated", "tonal", "outlined", "text"],description:"A predefined style for the Button."}  
   #!componentProp(material_3.ButtonMenu)!1: {name:"visible",type:"boolean",description:"If True, the component will be displayed."} 
   #!componentProp(material_3.ButtonMenu)!1: {name:"enabled",type:"boolean",description:"If True, this component allows user interaction."}
   #!componentProp(material_3.ButtonMenu)!1: {name:"role",type:"themeRole",description:"A style for this component defined in CSS and added to Roles"} 
   #!componentProp(material_3.ButtonMenu)!1: {name:"button_text_color",type:"color",description:"The colour of the text on the Button."} 
-  #!componentProp(material_3.ButtonMenu)!1: {name:"button_font_family",type:"string",description:"The font family to use for this component."}
-  #!componentProp(material_3.ButtonMenu)!1: {name:"icon",type:"enum",description:"The icon to display on this component."} 
-  #!componentProp(material_3.ButtonMenu)!1: {name:"text",type:"string",description:"The text displayed on this component"}
+  #!componentProp(material_3.ButtonMenu)!1: {name:"button_font_family",type:"string",description:"The font family to use for the Button"}
+  #!componentProp(material_3.ButtonMenu)!1: {name:"icon",type:"enum",description:"The icon to display on the Button."} 
+  #!componentProp(material_3.ButtonMenu)!1: {name:"text",type:"string",description:"The text displayed on the Button"}
   #!componentProp(material_3.ButtonMenu)!1: {name:"button_font_size",type:"number",description:"The font size of the text displayed on the Button."}
-  #!componentProp(material_3.ButtonMenu)!1: {name:"underline",type:"boolean",description:"If True, this component’s text will be underlined."}
-  #!componentProp(material_3.ButtonMenu)!1: {name:"italic",type:"boolean",description:"If True, this component’s text will be italic."}
-  #!componentProp(material_3.ButtonMenu)!1: {name:"bold",type:"boolean",description:"If True, this component’s text will be bold."}
-  #!componentProp(material_3.ButtonMenu)!1: {name:"icon_color",type:"color",description:"The colour of the icon displayed on this component."}
+  #!componentProp(material_3.ButtonMenu)!1: {name:"underline",type:"boolean",description:"If True, the Button’s text will be underlined."}
+  #!componentProp(material_3.ButtonMenu)!1: {name:"italic",type:"boolean",description:"If True, the Button’s text will be italic."}
+  #!componentProp(material_3.ButtonMenu)!1: {name:"bold",type:"boolean",description:"If True, the Button’s text will be bold."}
+  #!componentProp(material_3.ButtonMenu)!1: {name:"icon_color",type:"color",description:"The colour of the icon displayed on the Button."}
   #!componentProp(material_3.ButtonMenu)!1: {name:"menu_background_color",type:"color",description:"The colour of the menu."}
-  #!componentProp(material_3.ButtonMenu)!1: {name:"menu_border",type:"color",description:"The colour of the menu."}
+  #!componentProp(material_3.ButtonMenu)!1: {name:"menu_border",type:"color",description:"The border of the menu. Can take any valid CSS border value."}
   #!componentProp(material_3.ButtonMenu)!1: {name:"icon_size",type:"number",description:"The size (pixels) of the icon displayed on this component."}
   #!componentProp(material_3.ButtonMenu)!1: {name:"button_background_color",type:"color",description:"The colour of the background of the Button."}
   #!componentProp(material_3.ButtonMenu)!1: {name:"margin",type:"margin",description:"The margin (pixels) of the component."}
   #!componentProp(material_3.ButtonMenu)!1: {name:"button_border",type:"string",description:"The border of the Button. Can take any valid CSS border value."}
   #!componentProp(material_3.ButtonMenu)!1: {name:"tooltip",type:"string",description:"The text to display when the mouse is hovered over this component."}
   #!componentProp(material_3.ButtonMenu)!1: {name:"icon_position",type:"enum",options:["left", "right"],description:"The alignment of the icon on this component."}
+  #!componentProp(material_3.ButtonMenu)!1: {name:"menu_items",type:"object",description:"A list of components to be added to the menu."}
 
   #!componentEvent(material_3.ButtonMenu)!1: {name: "click", description: "When the component is clicked.", parameters:[]}
 
