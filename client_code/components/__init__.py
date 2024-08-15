6478d8c2aba56f790d6e60077f31eb9297c05a42
brooke@anvil.works <brooke@anvil.works>
2024-08-15 13:41:35 +0000

    Edited settings, components and test

--- client_code/components/__init__.py
+++ client_code/components/__init__.py
@@ -1,4 +1,4 @@
-from ._Components.Button import Button
+from .._Components.Button import Button
 from .._Components.ButtonMenu import ButtonMenu
 from .._Components.Card import Card
 from .._Components.Card.CardContentContainer import CardContentContainer
@@ -12,19 +12,38 @@ from .._Components.IconButton import IconButton
 from .._Components.InteractiveCard import InteractiveCard
 from .._Components.LinearProgressIndicator import LinearProgressIndicator
 from .._Components.Link import Link
-
-
-
-
-from .._Components.Text import Text
-
-from .._Components.TextInput import TextArea
-from .._Components.TextInput import TextField
 from .._Components.MenuItem import MenuItem
 from .._Components.NavigationLink import NavigationLink
-from .._Components.Switch import Switch
+from .._Components.RadioButton import RadioButton
 from .._Components.Slider import Slider
+from .._Components.Switch import Switch
+from .._Components.Text import Text
+from .._Components.TextInput import TextArea
+from .._Components.TextInput import TextField
 from .._Components.ToggleIconButton import ToggleIconButton
 
 
 Button = Button
+ButtonMenu = ButtonMenu
+Card = Card
+CardContentContainer = CardContentContainer
+Checkbox = Checkbox
+CircularProgressIndicator = CircularProgressIndicator
+Divider = Divider
+DropdownMenu = DropdownMenu
+FileLoader = FileLoader
+Heading = Heading
+IconButton = IconButton
+InteractiveCard = InteractiveCard
+LinearProgressIndicator = LinearProgressIndicator
+Link = Link
+MenuItem = MenuItem
+NavigationLink = NavigationLink
+RadioButton = RadioButton
+Slider = Slider
+Switch = Switch
+Text = Text
+TextArea = TextArea
+TextField = TextField
+ToggleIconButton = ToggleIconButton
+
--- client_code/test/__init__.py
+++ client_code/test/__init__.py
@@ -12,6 +12,7 @@ class test(testTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.column_panel_1.add_component(Button(text="Click me"))
     
 
     # Any code you write here will run before the form opens.
