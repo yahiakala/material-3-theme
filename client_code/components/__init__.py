3599b302cfcae506cd36494449d867f37f08c358
brooke@anvil.works <brooke@anvil.works>
2024-08-15 13:36:52 +0000

    Edited components

--- client_code/components/__init__.py
+++ client_code/components/__init__.py
@@ -1,23 +1,30 @@
 from ._Components.Button import Button
-from .._Components.Card import Card
-from .._Components.Link import Link
-from .._Components.IconButton import IconButton
 from .._Components.ButtonMenu import ButtonMenu
-from .._Components.Checkbox import Checkbox
+from .._Components.Card import Card
 from .._Components.Card.CardContentContainer import CardContentContainer
+from .._Components.Checkbox import Checkbox
 from .._Components.CircularProgressIndicator import CircularProgressIndicator
 from .._Components.Divider import Divider
 from .._Components.DropdownMenu import DropdownMenu
 from .._Components.FileLoader import FileLoader
 from .._Components.Heading import Heading
-from .._Components.Text import Text
+from .._Components.IconButton import IconButton
+from .._Components.InteractiveCard import InteractiveCard
 from .._Components.LinearProgressIndicator import LinearProgressIndicator
+from .._Components.Link import Link
+
+
+
+
+from .._Components.Text import Text
+
 from .._Components.TextInput import TextArea
 from .._Components.TextInput import TextField
 from .._Components.MenuItem import MenuItem
 from .._Components.NavigationLink import NavigationLink
 from .._Components.Switch import Switch
 from .._Components.Slider import Slider
-from .._Components
+from .._Components.ToggleIconButton import ToggleIconButton
+
 
 Button = Button
