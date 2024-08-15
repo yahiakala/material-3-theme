b19666fc58530ad9ce325b31456be1ba1cb78c63
brooke@anvil.works <brooke@anvil.works>
2024-08-15 13:34:27 +0000

    Created _Components, _Components.Button, _Components.ButtonMenu, _Components.Card and 26 more; Edited AnotherPage, BrookeTestPage, BrookeTestPage.RowTemplate2, Form2 and 26 more; Deleted Components, Components.Button, Components.ButtonMenu, Components.Card and 24 more

---
+++ client_code/test/__init__.py
@@ -0,0 +1,17 @@
+from ._anvil_designer import testTemplate
+from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+from ..components import Button
+
+
+
+class test(testTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    
+
+    # Any code you write here will run before the form opens.
