b2b0edf70017074705a2eaecce307729916ca385
brooke@anvil.works <brooke@anvil.works>
2024-10-07 10:53:05 +0000

    Edited _Components.TextInput.TextArea, AnotherPage, _Components.TextInput and _Components.TextInput.TextField

--- client_code/_Components/TextInput/TextArea.py
+++ client_code/_Components/TextInput/TextArea.py
@@ -203,7 +203,7 @@ class TextArea(TextInput):
   #!componentProp(material_3.TextArea)!1: {name:"border_color",type:"color",description:"The colour of the border of this component."}
   #!componentProp(material_3.TextArea)!1: {name:"placeholder",type:"string",description:"The text to be displayed when the component is empty"}
 
-  #!componentProp(material_3.TextArea)!1: {name:"spacing",type:"spacing",description:"The margin and padding of this component. Default value is in pixels."}
+  #!componentProp(material_3.TextArea)!1: {name:"margin",type:"margin",description:"The margin of this component. Default value is in pixels."}
   #!componentProp(material_3.TextArea)!1: {name:"tooltip",type:"string",description:"The text to display when the mouse is hovered over this component."}
   #!componentProp(material_3.TextArea)!1: {name:"input_text",type:"string",description:"The input text to display on this component"}
   #!componentProp(material_3.TextArea)!1: {name:"tag",type:"object",description:"Use this property to store any extra data for the component."}
