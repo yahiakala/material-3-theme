a8cf5f0d0f27b4d37db058a4592370aa9561ffb8
patricia@anvil.works <patricia@anvil.works>
2024-10-21 14:50:23 +0000

    Edited PatTestPage and _Components.TextInput

--- client_code/_Components/TextInput/__init__.py
+++ client_code/_Components/TextInput/__init__.py
@@ -18,7 +18,7 @@ class TextInput(TextInputTemplate):
     self._on_input = self._on_input
 
   def _anvil_get_unset_property_values_(self):
-    el = self.dom_nodes["anvil-m3-textinput"]
+    el = self.dom_nodes['anvil-m3-textinput']
     m = get_unset_margin(el, self.margin)
     label = self.dom_nodes['anvil-m3-label-text']
     supporting_text = self.dom_nodes['anvil-m3-supporting-text']
