1b37253f847ca82ee81c8f31b3a88c2981f8d74d
patricia@anvil.works <patricia@anvil.works>
2024-10-24 15:12:10 +0100

    Edited PatTestPage

--- client_code/PatTestPage/__init__.py
+++ client_code/PatTestPage/__init__.py
@@ -9,5 +9,11 @@ class PatTestPage(PatTestPageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    print(self.radio_group_panel_1.selected_button)
+    print(self.radio_group_panel_1.buttons)
 
     # Any code you write here will run before the form opens.
+
+  def radio_button_2_select(self, **event_args):
+    """This method is called when the radio button is selected."""
+    print(self.radio_group_panel_1.selected_button)
