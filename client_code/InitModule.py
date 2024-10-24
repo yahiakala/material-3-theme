94970d1e9296e9ecb5267131dacee17ee03caaba
patricia@anvil.works <patricia@anvil.works>
2024-10-24 14:43:22 +0100

    Edited InitModule

--- client_code/InitModule.py
+++ client_code/InitModule.py
@@ -1,14 +1,14 @@
 from anvil import pluggable_ui
 from ._Components.Button import Button
-from ._Components.TextInput.TextField import TextField
+from ._Components.TextInput.TextBox import TextBox
 from ._Components.Checkbox import Checkbox
 
 def make_footer_button(button_type, **kwargs):
   return Button(**kwargs)
 
 pluggable_ui.provide("m3", {
-  "anvil.TextBoxWithLabel": TextField,
-  "anvil.TextBox": TextField,
+  "anvil.TextBoxWithLabel": TextBox,
+  "anvil.TextBox": TextBox,
   "anvil.Button": Button,
   "anvil.CheckBox": Checkbox,
   "anvil.alerts.FooterButton": make_footer_button
