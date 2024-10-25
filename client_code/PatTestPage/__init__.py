db62c67daea75409905e9c6146ec4aee9b4d868d
brooke@anvil.works <brooke@anvil.works>
2024-10-25 15:20:17 +0000

    Edited PatTestPage

--- client_code/PatTestPage/__init__.py
+++ client_code/PatTestPage/__init__.py
@@ -9,6 +9,7 @@ class PatTestPage(PatTestPageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.checkbox_1.checked = None
 
     # Any code you write here will run before the form opens.
 
