c4c5716591ac1076e46dc6407a2523f1fe87366b
patricia@anvil.works <patricia@anvil.works>
2024-10-25 12:17:40 +0000

    Edited _Components.TextInput.TextBox

--- client_code/_Components/TextInput/TextBox.py
+++ client_code/_Components/TextInput/TextBox.py
@@ -3,7 +3,6 @@ from . import TextInput
 import anvil.designer
 from ...Functions import property_with_callback, italic_property, bold_property, underline_property, font_family_property, font_size_property, color_property
 from anvil.property_utils import anvil_property
-from ...utils import _m3_icons
 
 text_property = {"name": "text",
                  "type": "string",
