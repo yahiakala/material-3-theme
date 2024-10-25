79c185a325524353ed7c71a8114f0d91c236062c
patricia@anvil.works <patricia@anvil.works>
2024-10-25 15:14:10 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -10,13 +10,8 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.checkbox_1.checked = None
 
     # Any code you write here will run before the form opens.
 
-  def interactive_card_1_click(self, **event_args):
-    """This method is called when the component is clicked"""
-    alert()
-
-  def link_1_show(self, **event_args):
-    """This method is called when the component is shown on the screen."""
-    pass
+  
