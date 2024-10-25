6a5ec0eac2fde5c70630e522cc593cc14561227f
patricia@anvil.works <patricia@anvil.works>
2024-10-25 14:17:43 +0000

    Edited _Components.Card

--- client_code/_Components/Card/__init__.py
+++ client_code/_Components/Card/__init__.py
@@ -3,10 +3,10 @@ from anvil import *
 import anvil.server
 import anvil.designer
 from anvil import HtmlTemplate
-from ...Functions import tooltip_property, role_property, property_with_callback, style_property, spacing_property, margin_property, padding_property, color_property, border_property
+from ...Functions import tooltip_property, role_property, style_property, spacing_property, margin_property, padding_property, color_property, border_property
 from anvil.js import get_dom_node
 from anvil.js import window
-from ...utils.properties import get_unset_spacing
+from ...utils.properties import get_unset_spacing, anvil_prop
 
 class Card(CardTemplate):
   def __init__(self, **properties):
@@ -27,13 +27,15 @@ class Card(CardTemplate):
   def _set_class_of_nodes(self, appearance, val):
     self.dom_nodes['anvil-m3-card'].classList.toggle(f'anvil-m3-{appearance}', val)
     self.dom_nodes['content'].classList.toggle(f'anvil-m3-{appearance}', val)
-    
-  def _set_appearance(self, value):
+
+  @anvil_prop
+  def appearance(self, value):
     for appearance in ['outlined', 'filled', 'elevated']:
       self._set_class_of_nodes(appearance, False)
     self._set_class_of_nodes(value, True)
 
-  def _set_orientation(self, value):
+  @anvil_prop
+  def orientation(self, value):
     for c in ['anvil-m3-card-direction-column', 'anvil-m3-card-direction-row']:
       self.dom_nodes['anvil-m3-card'].classList.remove(c)
     self.dom_nodes['anvil-m3-card'].classList.add(f'anvil-m3-card-direction-{value}')
@@ -44,9 +46,7 @@ class Card(CardTemplate):
   role = role_property('anvil-m3-card')
   align = style_property('anvil-m3-card', 'justifyContent', 'align')
   visible = HtmlTemplate.visible
-  appearance = property_with_callback("appearance", _set_appearance)
   background_color = color_property('anvil-m3-card', 'backgroundColor', 'background_color')
-  orientation = property_with_callback('orientation', _set_orientation)
 
   #!componentProp(material_3.Card)!1: {name:"visible",type:"boolean",description:"If True, the component will be displayed."} 
   #!componentProp(material_3.Card)!1: {name:"border",type:"string",description:"The border of this component. Can take any valid CSS border value."}
