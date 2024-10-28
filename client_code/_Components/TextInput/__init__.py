bcbb6c6156ea717ff986e5afb175656627aa7ebd
patricia@anvil.works <patricia@anvil.works>
2024-10-28 12:21:10 +0000

    Edited _Components.TextInput

--- client_code/_Components/TextInput/__init__.py
+++ client_code/_Components/TextInput/__init__.py
@@ -33,7 +33,7 @@ class TextInput(TextInputTemplate):
   margin = margin_property('anvil-m3-textinput')
   tooltip = tooltip_property('anvil-m3-textinput')
   subcontent_color = color_property('anvil-m3-subcontent', 'color', 'subcontent_text_color')
-  subcontent_font_family = font_family_property('anvil-m3-subctontent', 'subcontent_font_family')
+  subcontent_font_family = font_family_property('anvil-m3-subcontent', 'subcontent_font_family')
   supporting_text_font_family = font_family_property('anvil-m3-supporting-text', 'supporting_text_font_family')
   supporting_text_font_size = font_size_property('anvil-m3-supporting-text', 'supporting_text_font_size')
   character_limit_font_family = font_family_property('anvil-m3-character-counter', 'character_limit_font_family')
