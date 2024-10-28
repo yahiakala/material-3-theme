efeb4308e7ea0bbce03b55824c2032b2d7732986
patricia@anvil.works <patricia@anvil.works>
2024-10-28 13:24:05 +0000

    Edited _utils.properties

--- client_code/_utils/properties.py
+++ client_code/_utils/properties.py
@@ -9,63 +9,6 @@ from anvil.property_utils import (
     get_padding_styles,
     get_spacing_styles,
 )
-
-_directions = ("Top", "Right", "Bottom", "Left")
-
-_style_getter = {"margin": get_margin_styles, "padding": get_padding_styles}
-
-def _as_style_array(styles, key):
-    return [styles[f"{key}{dir}"] for dir in _directions]
-
-def _get_value(s):
-    if s is None:
-        return s
-    if s.endswith("px"):
-        return float(s[:-2])
-
-def _get_unset(styles, key, current=None):
-    raw = _as_style_array(styles, key)
-    current_styles = _style_getter[key](current)
-    current = _as_style_array(current_styles, key)
-    raw = [None if v_curr else v_comp for v_curr, v_comp in zip(current, raw)]
-    return {"value": [_get_value(x) for x in raw], "css": raw}
-
-def get_unset_margin(element, current_value):
-    _get_unset(window.getComputedStyle(element), "margin", current_value)
-
-def get_unset_padding(element, current_value):
-    return _get_unset(window.getComputedStyle(element), "padding", current_value)
-
-def _get_current_spacing_key(current_value, key):
-    try:
-        return current_value[key]
-    except Exception:
-        return None
-
-def get_unset_spacing(element_margin, element_padding, current_value):
-    m_styles = window.getComputedStyle(element_margin)
-    p_styles = window.getComputedStyle(element_padding)
-    m = _get_unset(
-        m_styles, "margin", _get_current_spacing_key(current_value, "margin")
-    )
-    p = _get_unset(
-        p_styles, "padding", _get_current_spacing_key(current_value, "padding")
-    )
-    return {
-        "value": {"margin": m["value"], "padding": p["value"]},
-        "css": {"margin": m["css"], "padding": p["css"]},
-    }
-
-def get_unset_value(element, key, current_value):
-    if current_value is not None and current_value != "":
-        return {"value": None, "css": None}
-
-    styles = window.getComputedStyle(element)
-    css = styles[key]
-    return {"value": _get_value(css), "css": css}
-
-#TODO: after prod deploy, remove this try and delete the overwritten functions above
-
 from anvil.property_utils import (
     get_unset_margin,
     get_unset_padding,
@@ -73,6 +16,7 @@ from anvil.property_utils import (
     get_unset_value,
 )
 
+_directions = ("Top", "Right", "Bottom", "Left")
 
 class ComponentTag():
   def __repr__(self):
