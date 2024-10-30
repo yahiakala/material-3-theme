5dff2d191b2aead46c3ee29b91b044f8a5752e7d
stu <stu@anvil.works>
2024-10-30 11:16:28 +0800

    working on heading placeholders

--- client_code/_Components/Text/__init__.py
+++ client_code/_Components/Text/__init__.py
@@ -1,42 +1,80 @@
-from ._anvil_designer import TextTemplate
-from anvil import *
-import anvil.server
-from anvil import HtmlTemplate
+import anvil
 import anvil.designer
-from anvil.js.window import document
-from ..._utils import fui, noop
-from ..._utils.properties import get_unset_value, get_unset_spacing, anvil_prop,  tooltip_property, role_property, underline_property, italic_property, style_property, color_property, innerText_property, bold_property, font_size_property, font_family_property, border_property, spacing_property
-import time
+
+from ..._utils.properties import (
+  anvil_prop,
+  bold_property,
+  border_property,
+  color_property,
+  font_family_property,
+  font_size_property,
+  get_unset_spacing,
+  get_unset_value,
+  italic_property,
+  role_property,
+  spacing_property,
+  tooltip_property,
+  underline_property,
+)
+from ._anvil_designer import TextTemplate
+
 
 class Text(TextTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
-    self.tag = ComponentTag()
+    self.tag = anvil.ComponentTag()
     self._props = properties
     self._tooltip_node = None
     self.init_components(**properties)
-    
+
   def form_show(self, **event_args):
-    """This method is called when the HTML panel is shown on the screen"""
-    if anvil.designer.in_designer and not self.text:
-      self.text = anvil.designer.get_design_name(self)
+    self._set_designer_text_placeholder()
+
+  def _set_text(self, value):
+    self.dom_nodes['anvil-m3-text'].innerText = value
+
+  def _set_designer_text_placeholder(self, text=None):
+    if not anvil.designer.in_designer:
+      return
+
+    if text or self.text:
+      self.dom_nodes['anvil-m3-text'].style.opacity = ''
+      self.dom_nodes['anvil-m3-text'].style.fontStyle = 'italic' if self.italic else ''
+    else:
+      text = anvil.designer.get_design_name(self)
+      self._set_text(text)
+      self.dom_nodes['anvil-m3-text'].style.opacity = '0.5'
+      self.dom_nodes['anvil-m3-text'].style.fontStyle = 'italic'
+
+  def _start_inline_editing(self):
+    self._set_designer_text_placeholder(True)
+    if not self.text:
+      self._set_text("")
+    anvil.designer.start_inline_editing(
+      self,
+      "text",
+      self.dom_nodes["anvil-m3-text"],
+      on_finished=self._set_designer_text_placeholder,
+    )
 
   def _anvil_get_interactions_(self):
-    return [{
-      "type": "whole_component",
-      "title": "Edit text",
-      "icon": "edit",
-      "default": True,
-      "callbacks": {
-        "execute": lambda: anvil.designer.start_inline_editing(self, "text", self.dom_nodes['anvil-m3-text'])
+    return [
+      {
+        "type": "whole_component",
+        "title": "Edit text",
+        "icon": "edit",
+        "default": True,
+        "callbacks": {"execute": self._start_inline_editing},
       }
-    }]
+    ]
 
   def _anvil_get_unset_property_values_(self):
     el = self.dom_nodes["anvil-m3-text-container"]
     sp = get_unset_spacing(el, el, self.spacing)
     tfs = get_unset_value(self.dom_nodes['anvil-m3-text'], "fontSize", self.font_size)
-    ifs = get_unset_value(self.dom_nodes['anvil-m3-text-icon'], "fontSize", self.icon_size)
+    ifs = get_unset_value(
+      self.dom_nodes['anvil-m3-text-icon'], "fontSize", self.icon_size
+    )
     return {"font_size": tfs, "icon_size": ifs, "spacing": sp}
 
   #!componentEvent(m3.Text)!1: {name: "show", description: "When the Text is shown on the screen."}
@@ -48,7 +86,7 @@ class Text(TextTemplate):
   #!componentProp(m3.Text)!1: {name:"bold",type:"boolean",description:"If True, this component’s text will be bold."}
   #!componentProp(m3.Text)!1: {name:"border",type:"string",description:"The border of this component. Can take any valid CSS border value."}
   #!componentProp(m3.Text)!1: {name:"font_family",type:"string",description:"The font family to use for this component."}
-  #!componentProp(m3.Text)!1: {name:"text_color",type:"color",description:"The color of the text on the component."} 
+  #!componentProp(m3.Text)!1: {name:"text_color",type:"color",description:"The color of the text on the component."}
   #!componentProp(m3.Text)!1: {name:"icon_color",type:"color",description:"The color of the icon displayed on this component."}
   #!componentProp(m3.Text)!1: {name:"background_color",type:"color",description:"The color of the background of this component."}
   #!componentProp(m3.Text)!1: {name:"text",type:"string",description:"The text displayed on this component"}
@@ -56,16 +94,16 @@ class Text(TextTemplate):
   #!componentProp(m3.Text)!1: {name:"spacing",type:"spacing",description:"The margin and padding (pixels) of the component."}
   #!componentProp(m3.Text)!1: {name:"tooltip",type:"string",description:"The text to display when the mouse is hovered over this component."}
   #!componentProp(m3.Text)!1: {name:"role",type:"themeRole",description:"A style for this component defined in CSS and added to Roles"}
-  #!componentProp(m3.Text)!1: {name:"align",type:"enum",options:['left', 'right', 'center', 'justify'], description:"The position of this component in the available space."} 
+  #!componentProp(m3.Text)!1: {name:"align",type:"enum",options:['left', 'right', 'center', 'justify'], description:"The position of this component in the available space."}
   #!componentProp(m3.Text)!1: {name:"font_size",type:"number",description:"The font size of text displayed on this component."}
-  #!componentProp(m3.Text)!1: {name:"icon",type:"enum",description:"The icon to display on this component."} 
+  #!componentProp(m3.Text)!1: {name:"icon",type:"enum",description:"The icon to display on this component."}
   #!componentProp(m3.Text)!1: {name:"line_height",type:"string",description:"The line height of this component."}
-  #!componentProp(m3.Text)!1: {name:"material_icon",type:"enum",description:"The icon to display on this component."} 
-  #!componentProp(m3.Text)!1: {name:"style",type:"enum",options:['display', 'headline', 'title'],description:"Role of the text component: display, headline or title."}  
+  #!componentProp(m3.Text)!1: {name:"material_icon",type:"enum",description:"The icon to display on this component."}
+  #!componentProp(m3.Text)!1: {name:"style",type:"enum",options:['display', 'headline', 'title'],description:"Role of the text component: display, headline or title."}
   #!componentProp(m3.Text)!1: {name:"scale",type:"enum",options:['small', 'medium', 'large'],description:"The size of the text component."}
   #!componentProp(m3.Text)!1: {name:"tag",type:"object",description:"Use this property to store any extra data for the component."}
 
-  visible = HtmlTemplate.visible
+  visible = anvil.HtmlTemplate.visible
   underline = underline_property('anvil-m3-text')
   italic = italic_property('anvil-m3-text')
   bold = bold_property('anvil-m3-text')
@@ -73,12 +111,18 @@ class Text(TextTemplate):
   font_family = font_family_property('anvil-m3-text', 'font_family')
   text_color = color_property('anvil-m3-text-container', 'color', 'text_color')
   icon_color = color_property('anvil-m3-text-icon', 'color', 'icon_color')
-  background_color = color_property('anvil-m3-text-container', 'backgroundColor', 'background_color')
-  text = innerText_property('anvil-m3-text')
+  background_color = color_property(
+    'anvil-m3-text-container', 'backgroundColor', 'background_color'
+  )
   icon_size = font_size_property('anvil-m3-text-icon', 'icon_size')
   spacing = spacing_property('anvil-m3-text-container')
   tooltip = tooltip_property('anvil-m3-text-container')
   role = role_property('anvil-m3-text-container')
+
+  @anvil_prop
+  def text(self, value):
+    self._set_text(value)
+    self._set_designer_text_placeholder()
 
   @anvil_prop
   def align(self, value):
@@ -90,7 +134,8 @@ class Text(TextTemplate):
 
   @anvil_prop
   def font_size(self, value):
-    if value: value = f'{value}px'
+    if value:
+      value = f'{value}px'
     self.dom_nodes['anvil-m3-text'].style.fontSize = value
     self.dom_nodes['anvil-m3-text-container'].style.fontSize = value
 
@@ -109,16 +154,25 @@ class Text(TextTemplate):
 
   @anvil_prop
   def style(self, value):
-    self.dom_nodes['anvil-m3-text'].classList.remove('anvil-m3-text-label', 'anvil-m3-text-body')
+    self.dom_nodes['anvil-m3-text'].classList.remove(
+      'anvil-m3-text-label', 'anvil-m3-text-body'
+    )
     self.dom_nodes['anvil-m3-text'].classList.add(f'anvil-m3-text-{value}')
-    self.dom_nodes['anvil-m3-text-container'].classList.remove('anvil-m3-text-label', 'anvil-m3-text-body')
+    self.dom_nodes['anvil-m3-text-container'].classList.remove(
+      'anvil-m3-text-label', 'anvil-m3-text-body'
+    )
     self.dom_nodes['anvil-m3-text-container'].classList.add(f'anvil-m3-text-{value}')
 
   @anvil_prop
   def scale(self, value):
-    self.dom_nodes['anvil-m3-text'].classList.remove('anvil-m3-text-large', 'anvil-m3-text-medium', 'anvil-m3-text-small')
-    self.dom_nodes['anvil-m3-text-container'].classList.remove('anvil-m3-text-large', 'anvil-m3-text-medium', 'anvil-m3-text-small')
+    self.dom_nodes['anvil-m3-text'].classList.remove(
+      'anvil-m3-text-large', 'anvil-m3-text-medium', 'anvil-m3-text-small'
+    )
+    self.dom_nodes['anvil-m3-text-container'].classList.remove(
+      'anvil-m3-text-large', 'anvil-m3-text-medium', 'anvil-m3-text-small'
+    )
     self.dom_nodes['anvil-m3-text'].classList.add(f'anvil-m3-text-{value}')
     self.dom_nodes['anvil-m3-text-container'].classList.add(f'anvil-m3-text-{value}')
 
-#!defClass(m3, Text, anvil.Component)!:      
+
+#!defClass(m3, Text, anvil.Component)!:
