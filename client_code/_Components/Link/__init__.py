452fe2501f6ba7f4bfd061ff840fb63da0fdf523
stu <stu@anvil.works>
2024-11-11 21:00:15 +0800

    Format link compoent

--- client_code/_Components/Link/__init__.py
+++ client_code/_Components/Link/__init__.py
@@ -1,6 +1,5 @@
+import anvil
 import anvil.designer
-from anvil import *
-from anvil import HtmlTemplate
 
 from ..._utils.properties import (
   anvil_prop,
@@ -22,7 +21,7 @@ class Link(LinkTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.temp_url = None
-    self.tag = ComponentTag()
+    self.tag = anvil.ComponentTag()
     self._props = properties
     self._tooltip_node = None
     self.init_components(**properties)
@@ -34,24 +33,40 @@ class Link(LinkTemplate):
 
   def form_show(self, **event_args):
     """This method is called when the HTML panel is shown on the screen"""
-    if anvil.designer.in_designer and not self.text and not self.icon and not self.get_components():
-      self.dom_nodes['anvil-m3-link-text'].innerText = anvil.designer.get_design_name(self)
+    if (
+      anvil.designer.in_designer
+      and not self.text
+      and not self.icon
+      and not self.get_components()
+    ):
+      self.dom_nodes['anvil-m3-link-text'].innerText = anvil.designer.get_design_name(
+        self
+      )
       self.dom_nodes['anvil-m3-link-text'].style.display = 'block'
 
   def _handle_click(self, event):
-    keys = {'shift': event.shiftKey, 'alt': event.altKey, 'ctrl': event.ctrlKey, 'meta': event.metaKey}
+    keys = {
+      'shift': event.shiftKey,
+      'alt': event.altKey,
+      'ctrl': event.ctrlKey,
+      'meta': event.metaKey,
+    }
     self.raise_event("click", keys=keys, event=event)
 
   def _anvil_get_interactions_(self):
-    return [{
-      "type": "whole_component",
-      "title": "Edit text",
-      "icon": "edit",
-      "default": True,
-      "callbacks": {
-        "execute": lambda: anvil.designer.start_inline_editing(self, "text", self.dom_nodes['anvil-m3-link-text'])
+    return [
+      {
+        "type": "whole_component",
+        "title": "Edit text",
+        "icon": "edit",
+        "default": True,
+        "callbacks": {
+          "execute": lambda: anvil.designer.start_inline_editing(
+            self, "text", self.dom_nodes['anvil-m3-link-text']
+          )
+        },
       }
-    }]
+    ]
 
   def _revoke_tmp_url(self):
     if self.temp_url:
@@ -62,20 +77,20 @@ class Link(LinkTemplate):
   #!componentEvent(m3.Link)!1: {name: "hide", description: "When the Link is removed from the screen."}
 
   #!componentProp(m3.Link)!1: {name:"text",type:"string",description:"The text displayed on this component."}
-  #!componentProp(m3.Link)!1: {name:"align",type:"enum", options:["left", "center", "right"],description:"The position of this component in the available space."} 
+  #!componentProp(m3.Link)!1: {name:"align",type:"enum", options:["left", "center", "right"],description:"The position of this component in the available space."}
   #!componentProp(m3.Link)!1: {name:"italic",type:"boolean",description:"If True, this component’s text will be italic."}
   #!componentProp(m3.Link)!1: {name:"bold",type:"boolean",description:"If True, this component’s text will be bold."}
   #!componentProp(m3.Link)!1: {name:"underline",type:"boolean",description:"If True, this component’s text will be underlined."}
   #!componentProp(m3.Link)!1: {name:"font_family",type:"string",description:"The font family to use for this component."}
   #!componentProp(m3.Link)!1: {name:"font_size",type:"number",description:"The font size of text displayed on this component."}
-  #!componentProp(m3.Link)!1: {name:"icon",type:"enum",description:"The icon to display on this component."} 
-  #!componentProp(m3.Link)!1: {name:"visible",type:"boolean",description:"If True, the component will be displayed."} 
-  #!componentProp(m3.Link)!1: {name:"text_color",type:"color",description:"The color of the text on the component."} 
+  #!componentProp(m3.Link)!1: {name:"icon",type:"enum",description:"The icon to display on this component."}
+  #!componentProp(m3.Link)!1: {name:"visible",type:"boolean",description:"If True, the component will be displayed."}
+  #!componentProp(m3.Link)!1: {name:"text_color",type:"color",description:"The color of the text on the component."}
   #!componentProp(m3.Link)!1: {name:"icon_color",type:"color",description:"The color of the icon displayed on this component."}
   #!componentProp(m3.Link)!1: {name:"border",type:"string",description:"The border of this component. Can take any valid CSS border value."}
   #!componentProp(m3.Link)!1: {name:"spacing",type:"spacing",description:"The margin and padding (pixels) of the component."}
   #!componentProp(m3.Link)!1: {name:"tooltip",type:"string",description:"The text to display when the mouse is hovered over this component."}
-  #!componentProp(m3.Link)!1: {name:"role",type:"themeRole",description:"A style for this component defined in CSS and added to Roles."} 
+  #!componentProp(m3.Link)!1: {name:"role",type:"themeRole",description:"A style for this component defined in CSS and added to Roles."}
   #!componentProp(m3.Link)!1: {name:"url",type:"string",description:"TThe target URL of the link. Can be set to a URL string or to a Media object."}
   #!componentProp(m3.Link)!1: {name:"icon_size",type:"number",description:"The size (pixels) of the icon displayed on this component."}
   #!componentProp(m3.Link)!1: {name:"tag",type:"object",description:"Use this property to store any extra data for the component."}
@@ -86,15 +101,17 @@ class Link(LinkTemplate):
   font_family = font_family_property('anvil-m3-link', 'font_family')
   font_size = font_size_property('anvil-m3-link')
   underline = underline_property('anvil-m3-link-text')
-  visible = HtmlTemplate.visible
+  visible = anvil.HtmlTemplate.visible
   text_color = color_property('anvil-m3-link', 'color', 'text_color')
   icon_color = color_property('anvil-m3-link-icon', 'color', 'icon_color')
   border = style_property('anvil-m3-link', 'border', 'border')
   spacing = spacing_property('anvil-m3-link')
   tooltip = tooltip_property('anvil-m3-link')
   role = role_property('anvil-m3-link')
-  background_color = color_property('anvil-m3-link', 'backgroundColor', 'background_color')
- 
+  background_color = color_property(
+    'anvil-m3-link', 'backgroundColor', 'background_color'
+  )
+
   @anvil_prop(default_value='left')
   def align(self, value):
     self.dom_nodes['anvil-m3-link'].style.textAlign = value
@@ -105,8 +122,8 @@ class Link(LinkTemplate):
     self.dom_nodes['anvil-m3-link'].removeAttribute("download")
     self.dom_nodes['anvil-m3-link'].removeAttribute("target")
     self._revoke_tmp_url()
-    if value: 
-      if isinstance(value, Media):
+    if value:
+      if isinstance(value, anvil.Media):
         if value.name:
           self.dom_nodes['anvil-m3-link'].setAttribute("download", value.name)
         self.temp_url = anvil.media.TempUrl(value)
@@ -138,5 +155,6 @@ class Link(LinkTemplate):
       self.dom_nodes['anvil-m3-link-text'].style.display = 'block'
     else:
       self.dom_nodes['anvil-m3-link-text'].style.display = 'none'
+
 
 #!defClass(m3, Link, anvil.Component)!:
